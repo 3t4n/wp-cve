d903620a1ea7221d7dcc7fa36be353a04e64eb31
root <root@vultr>
2024-03-14 14:23:57 +0000

    Added plugin simple-google-adsense

---
+++ plugins/simple-google-adsense/includes/class-simple-google-adsense-admin.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Simple_Google_Adsense admin setup
+ *
+ * @package Simple_Google_Adsense
+ * @since   1.0.0
+ */
+
+defined('ABSPATH') || exit;
+
+/**
+ * Main Simple_Google_Adsense_Admin Class.
+ *
+ * @class Simple_Google_Adsense
+ */
+final class Simple_Google_Adsense_Admin
+{
+
+    /**
+     * The single instance of the class.
+     *
+     * @var Simple_Google_Adsense_Admin
+     * @since 1.0.0
+     */
+    protected static $_instance = null;
+
+
+    /**
+     * Main Simple_Google_Adsense_Admin Instance.
+     *
+     * Ensures only one instance of Simple_Google_Adsense_Admin is loaded or can be loaded.
+     *
+     * @return Simple_Google_Adsense_Admin - Main instance.
+     * @since 1.0.0
+     * @static
+     */
+    public static function instance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Simple_Google_Adsense Constructor.
+     */
+    public function __construct()
+    {
+        $this->includes();
+        $this->init_hooks();
+    }
+
+    /**
+     * Hook into actions and filters.
+     *
+     * @since 1.0.0
+     */
+    private function init_hooks()
+    {
+
+        add_filter('plugin_action_links', array($this, 'action_links'), 10, 2);
+        add_action('admin_init', array($this, 'settings'));
+        add_action('admin_menu', array($this, 'option_menu'));
+
+
+    }
+
+
+    public function action_links($links, $file)
+    {
+        if ($file == SIMPLE_GOOGLE_ADSENSE_BASENAME) {
+            $links[] = '<a href="options-general.php?page=simple-google-adsense-settings">' . __('Settings', 'simple-google-adsense') . '</a>';
+        }
+        return $links;
+
+    }
+
+    function sanitize($input)
+    {
+        $sanitized_input = array();
+        if (isset($input['publisher_id'])) {
+            $sanitized_input['publisher_id'] = sanitize_text_field($input['publisher_id']);
+        }
+        return $sanitized_input;
+    }
+
+    public function settings()
+    {
+        $args = array(
+            'sanitize_callback' => array($this, 'sanitize')
+        );
+        register_setting('simple_google_adsense_page', 'simple_google_adsense_settings', $args);
+
+        add_settings_section(
+            'simple_google_adsense_section',
+            __('General Settings', 'simple-google-adsense'),
+            array($this, 'setting_section_callback'),
+            'simple_google_adsense_page'
+        );
+
+        add_settings_field(
+            'publisher_id',
+            __('Publisher ID', 'simple-google-adsense'),
+            array($this, 'publisher_id_render'),
+            'simple_google_adsense_page',
+            'simple_google_adsense_section'
+        );
+    }
+
+    function publisher_id_render()
+    {
+        $options = get_option('simple_google_adsense_settings');
+        $publisher_id = isset($options['publisher_id']) ? $options['publisher_id'] : '';
+        ?>
+        <input type='text' name='simple_google_adsense_settings[publisher_id]'
+               value='<?php echo esc_attr($publisher_id) ?>'>
+        <p class="description"><?php printf(__('Enter your Google AdSense Publisher ID (e.g %s).', 'simple-google-adsense'), 'pub-1234567890123456'); ?></p>
+        <?php
+    }
+
+    public function option_menu()
+    {
+        if (is_admin()) {
+            add_options_page(__('Google AdSense', 'simple-google-adsense'),
+                __('Google AdSense', 'simple-google-adsense'), 'manage_options',
+                'simple-google-adsense-settings', array($this, 'options_page'));
+        }
+    }
+
+    function options_page()
+    {
+        ?>
+        <div class="wrap">
+            <h2><?php __('Simple Google Adsense', 'simple-google-adsense') ?></h2>
+            <form action='options.php' method='post'>
+                <?php
+                settings_fields('simple_google_adsense_page');
+                do_settings_sections('simple_google_adsense_page');
+                submit_button();
+                ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    public function setting_section_callback()
+    {
+
+    }
+
+    /**
+     * Include required core files used in frontend.
+     */
+    public function includes()
+    {
+
+        include_once SIMPLE_GOOGLE_ADSENSE_ABSPATH . 'includes/admin/dashboard/class-mantrabrain-admin-dashboard.php';
+
+        if (!class_exists('Htaccess_File_Editor_Ebwp_Notice')) {
+            include_once SIMPLE_GOOGLE_ADSENSE_ABSPATH . 'includes/class-simple-google-adsense-notice.php';
+        }
+    }
+
+
+}
---
+++ plugins/simple-google-adsense/readme.txt
@@ -0,0 +1,101 @@
+=== Simple Google AdSense - Simple & Best Google AdSense Plugin for WordPress ===
+Contributors: MantraBrain
+Tags: google,adsense, google ad
+Requires at least: 4.9
+Tested up to: 6.3
+Requires PHP: 5.2.4
+Stable tag: 1.0.11
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add google adsense code to your WordPress site. No need to configure too much, just put publisher ID from Settings->Google AdSense.
+
+== Description ==
+
+Add google adsense code to your WordPress site. No need to configure too much, just put publisher ID from Settings->Google AdSense.
+
+=== What is Google AdSense Auto Ads? ===
+
+Auto Ads is an improved monetization method in Google AdSense. It uses machine learning to make smart ad placement for your website. When this is enabled, Google will analyze your site and show ads only when it thinks they will perform well and provide a good user experience.
+
+== MORE PRODUCTS FROM US ==
+>**[Yatra WordPress Travel Booking Plugin](https://wpyatra.com/?ref=wporghtaccess)**
+>Yatra is a free, SEO friendly & scalable WordPress travel booking plugin to create travel and tour packages for tour operators and travel agencies. [View Details](https://wpyatra.com/?ref=wporghtaccess)
+
+>**[Ultimate Image Watermark](https://wordpress.org/plugins/ultimate-watermark/)**
+>Ultimate Watermark is WordPress Image Watermark plugin. It can automatically add watermark to any images as they are uploaded to WordPress media library and bulk watermark previously uploaded images. [View Details](https://wordpress.org/plugins/ultimate-watermark/)
+
+
+=== How to Use Simple Google AdSense ===
+
+1. Install and Activate the *Simple Google AdSense* plugin
+1. Login to Google AdSense account
+1. Go to **Settings > Account > Account information** and copy your **AdSense Publisher ID**
+1. Login to your WordPress admin dashboard and go to **Settings > Simple Google AdSense**.
+1. Paste your AdSense Publisher ID into the **Publisher ID** field
+1. Click **Save Changes**
+1. Go back to your Google AdSense account dashboard and navigate to **My ads > Content > Auto ads**.
+1. Select ad types that you wish to automatically show on your website
+1. Click **SAVE**
+
+
+== Installation ==
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select simple-google-adsense.zip from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download simple-google-adsense.zip
+2. Extract the simple-google-adsense directory to your computer
+3. Upload the simple-google-adsense directory to the /wp-content/plugins/ directory
+4. Activate the plugin in the Plugin dashboard
+
+= Can I use this plugin if I have already configure google adsence on my WordPress site? =
+
+Yes.
+
+== Screenshots ==
+
+1. Setting Page
+
+
+== Changelog ==
+
+= 1.0.11 | 2023/07/22
+ - WordPress 6.3 tested
+
+ = 1.0.10 | 2022/11/06
+ - WordPress 6.1 tested
+
+ = 1.0.9 | 2022/06/20
+ - WordPress 6.0 tested
+
+= 1.0.7 | 2022/05/10
+ - WordPress 6.0 tested
+
+ = 1.0.6 | 2021/07/24
+ - WordPress 5.8 tested
+
+= 1.0.5 | 2020/12/15
+ - Version tested
+
+= 1.0.4 | 2020/02/20
+ - Version tested
+
+= 1.0.3 | 2019/12/10
+ - Version tested
+ = 1.0.2 | 2019/07/21
+
+ - Minor fixing & dashboard widget added
+
+ = 1.0.1 | 2019/05/18
+ - Minor fixing
+
+= 1.0.0 | 2019/04/27
+ - Initial Release
