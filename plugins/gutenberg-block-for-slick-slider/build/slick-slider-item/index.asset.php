b88bf8d416fc56b969bca294b946c8511e66dca5
root <root@vultr>
2024-03-14 15:59:47 +0000

    Added plugin gutenberg-block-for-slick-slider

---
+++ plugins/gutenberg-block-for-slick-slider/build/slick-slider-item/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => '7cf54c7a8e651a20a340');
---
+++ plugins/gutenberg-block-for-slick-slider/build/slick-slider/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4f12b7ea034492c7eed0');
