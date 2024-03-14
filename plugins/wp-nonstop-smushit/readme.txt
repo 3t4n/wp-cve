d3c9e5bb669d4cd22e4036c8b8e666d79e977161
root <root@vultr>
2024-03-14 03:06:34 +0000

    Added plugin wp-nonstop-smushit

---
+++ plugins/wp-nonstop-smushit/readme.txt
@@ -0,0 +1,51 @@
+=== Disable Bulk Smush Limit of WP Smushit ===
+Plugin Name: Disable Bulk Smush Limit of WP Smushit
+Version: 2.0.0
+Author: obiPlabon
+Author URI: https://obiPlabon.im/
+Contributors: obiplabon
+Tags: wp smushit, image, compression, performance, optimization, disable
+Requires at least: 4.6
+Tested up to: 5.2.4
+Requires PHP: 5.4
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Disable bulk smush limit and enjoy a premium feature of ***WP Smushit*** completely FREE 😉
+
+== Description ==
+[WP Smushit](https://wordpress.org/plugins/wp-smushit/ "Smush Image Compression and Optimization") is a great WordPress plugin 😇 to compress and optimize images for better performance. Unsurprisingly it's not completely free and it should not be because of its awesome features. But I just wanted the bulk compression feature for FREE. So, I created this add-on plugin and it does the job so well that I don't need to think about clicking the smush button again.
+
+Note: ***WP Nonstop Smushit*** doesn't have any user interface or UI.
+
+Github: [WP Nonstop Smushit](https://github.com/obiPlabon/wp-nonstop-smushit)
+
+== Frequently Asked Questions ==
+
+= Is it FREE? =
+
+Yes, it's completely free forever.
+
+= Will there be any problem from WP Smushit? ==
+
+== Changelog ==
+
+= 2.0.0 =
+
+* Update: No major update but the version number 😁
+* Fix: Compatibility issue with the latest **WP Smushit**
+
+= 1.0.1 =
+
+* Update: Version updated with minor change
+
+= 1.0.0 =
+
+* Initial release
+
+No worries, there will not be any problem form WP Smushit.
+
+== About ==
+
+Hello, This is Obi from Bangladesh with 5 years of WordPress development experience and focus on JavaScript, WooCommerce and Elementor. Currently working on an awesome Elementor project called [HappyAddons](https://wordpress.org/plugins/happy-elementor-addons/), don't forget to check those mind blowing designs! And I am available for freelance work and if you need any assistance with your WordPress project feel free share and say 👋 (hi, hello) on facebook: [https://fb.me/obiPlabon](https://fb.me/obiPlabon)
---
+++ plugins/wp-nonstop-smushit/wp-nonstop-smushit.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * WP Nonstop Smushit plugin
+ *
+ * Disable bulk smash limit and enjoy a premium feature of WP Smashit completely FREE!
+ *
+ * @link        https://github.com/obiPlabon/wp-nonstop-smushit
+ * @since       1.0.0
+ * @package     WP_Nonstop_Smushit
+ *
+ * Plugin Name: Smush Nonstop
+ * Plugin URI:  https://github.com/obiPlabon/wp-nonstop-smushit
+ * Description: Disable bulk smash limit and enjoy one of the most exciting premium feature of <a href="https://wordpress.org/plugins/wp-smushit/" target="_blank">WP Smashit</a> completely FREE 😉
+ * Version:     2.0.0
+ * Author:      obiPlabon
+ * Author URI:  https://obiPlabon.im/
+ * License:     GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-nonstop-smushit
+ * Domain Path: /languages/
+ */
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2019 obiPlabon <https://obiPlabon.im>
+*/
+
+defined( 'ABSPATH' ) || die();
+
+if ( ! class_exists( 'WP_Nonstop_Smushit' ) ) {
+    class WP_Nonstop_Smushit {
+
+        /**
+         * Plugin version number
+         */
+        const VERSION = '2.0.0';
+
+        /**
+         * Plugin slug
+         */
+        const SLUG = 'wp-nonstop-smushit';
+
+        /**
+         * WP_Nonstop_Smushit instance
+         *
+         * @var null
+         */
+        protected static $instance = null;
+
+        /**
+         * Get instance
+         *
+         * @return null|WP_Nonstop_Smushit
+         */
+        public static function get_instance() {
+            if ( is_null( self::$instance ) ) {
+                self::$instance = new self();
+            }
+            return self::$instance;
+        }
+
+        /**
+         * WP_Nonstop_Smushit constructor.
+         */
+        protected function __construct() {
+            if ( ! $this->has_wp_smushit() ) {
+                add_action( 'admin_notices', [ $this, 'show_dependency_missing_error' ] );
+            } else {
+                add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+            }
+        }
+
+        /**
+         * Check the existence of WP Smushit plugin
+         * @return bool
+         */
+        protected function has_wp_smushit() {
+            return defined( 'WP_SMUSH_VERSION' ) || class_exists( '\Smush\WP_Smush' ) || class_exists( 'WP_Smush' );
+        }
+
+        /**
+         * Show dependency missing error message.
+         *
+         * @return void
+         */
+        public function show_dependency_missing_error() {
+            ?>
+            <div class="notice notice-error is-dismissible">
+                <p><?php printf(
+                        esc_html__( '%1$s requires %2$s to be installed. Please install %2$s otherwise there is no point in installing %1$s.', 'wp-nonstop-smushit' ),
+                        '<strong>' . esc_html__( 'WP Nonstop Smushit', 'wp-nonstop-smushit' ) . '</strong>',
+                        '<mark>' . esc_html__( 'WP Smushit', 'wp-nonstop-smushit' ) . '</mark>'
+                    ); ?></p>
+            </div>
+            <?php
+        }
+
+        /**
+         * Enqueue required assets.
+         *
+         * @param $page
+         *
+         * @return void
+         */
+        public function enqueue_scripts( $page ) {
+            if ( $page === 'toplevel_page_smush' ) {
+                wp_enqueue_script(
+                    self::SLUG,
+                    plugin_dir_url( __FILE__ ) . 'assets/js/main.js',
+                    null,
+                    self::VERSION,
+                    true
+                );
+            }
+        }
+    }
+}
+
+add_action( 'plugins_loaded', [ 'WP_Nonstop_Smushit', 'get_instance' ], 20 );
