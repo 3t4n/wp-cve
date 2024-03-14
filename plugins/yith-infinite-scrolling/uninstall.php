bdbd2c5d0549d97196e1281c1ee7a086cfb06dc6
root <root@vultr>
2024-03-14 16:30:04 +0000

    Added plugin yith-infinite-scrolling

---
+++ plugins/yith-infinite-scrolling/uninstall.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Uninstall plugin
+ *
+ * @author YITH <plugins@yithemes.com>
+ * @package YITH Infinite Scrolling
+ * @version 1.0.0
+ */
+
+// If uninstall not called from WordPress exit.
+if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
