731798ff73040a8292e9341b8837092bf5434b6f
root <root@vultr>
2024-03-14 13:24:18 +0000

    Added plugin composite-products-conditional-images-for-woocommerce

---
+++ plugins/composite-products-conditional-images-for-woocommerce/composite-products-conditional-images-for-woocommerce.php
@@ -0,0 +1,379 @@
+<?php
+/**
+* Plugin Name: Composite Products - Conditional Images
+* Plugin URI: https://docs.woocommerce.com/document/composite-products/composite-products-extensions/#cp-ci
+* Description: Free mini-extension for WooCommerce Composite Products that allows you to create dynamic, multi-layer Composite Product images that respond to option changes.
+* Version: 1.3.0
+* Author: SomewhereWarm
+* Author URI: https://somewherewarm.com/
+*
+* Text Domain: woocommerce-composite-products-conditional-images
+* Domain Path: /languages/
+*
+* Requires at least: 4.4
+* Tested up to: 6.3
+*
+* WC requires at least: 3.1
+* WC tested up to: 8.0
+*
+* Copyright: © 2017-2021 SomewhereWarm SMPC.
+* License: GNU General Public License v3.0
+* License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Main plugin class.
+ *
+ * @class    WC_CP_Conditional_Images
+ * @version  1.3.0
+ */
+class WC_CP_Conditional_Images {
+
+	/**
+	 * Plugin version.
+	 *
+	 * @var string
+	 */
+	public static $version = '1.2.6';
+
+	/**
+	 * Min required CP version.
+	 *
+	 * @var string
+	 */
+	public static $req_cp_version = '5.1';
+
+	/**
+	 * CP URL.
+	 *
+	 * @var string
+	 */
+	private static $cp_url = 'https://woocommerce.com/products/composite-products/';
+
+	/**
+	 * Plugin URL.
+	 *
+	 * @return string
+	 */
+	public static function plugin_url() {
+		return plugins_url( basename( plugin_dir_path(__FILE__) ), basename( __FILE__ ) );
+	}
+
+	/**
+	 * Plugin path.
+	 *
+	 * @return string
+	 */
+	public static function plugin_path() {
+		return untrailingslashit( plugin_dir_path( __FILE__ ) );
+	}
+
+	/**
+	 * Fire in the hole!
+	 */
+	public static function init() {
+		add_action( 'plugins_loaded', array( __CLASS__, 'load_plugin' ) );
+	}
+
+	/**
+	 * Hooks.
+	 */
+	public static function load_plugin() {
+
+		if ( ! function_exists( 'WC_CP' ) || version_compare( WC_CP()->version, self::$req_cp_version ) < 0 ) {
+			add_action( 'admin_notices', array( __CLASS__, 'cp_version_check_notice' ) );
+			return false;
+		}
+
+		// Declare HPOS compatibility.
+		add_action( 'before_woocommerce_init', array( __CLASS__, 'declare_hpos_compatibility' ) );
+
+		// Localization.
+		add_action( 'init', array( __CLASS__, 'localize_plugin' ) );
+
+		// Front-end script (where the magic happens).
+		add_filter( 'woocommerce_composite_script_dependencies', array( __CLASS__, 'frontend_script' ) );
+
+		// If overlays exist, dequeue zoom script.
+		add_action( 'woocommerce_composite_add_to_cart', array( __CLASS__, 'dequeue_zoom_script' ), 10 );
+
+		// Admin script.
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'admin_scripts' ) );
+
+		// Add 'Overlay Image' action in Scenarios.
+		add_action( 'woocommerce_composite_scenario_admin_actions_html', array( __CLASS__, 'scenario_admin_actions_html' ), 20, 4 );
+
+		// Save 'Overlay Image' action settings.
+		add_filter( 'woocommerce_composite_process_scenario_data', array( __CLASS__, 'process_scenario_data' ), 10, 5 );
+
+		// Add qty data in scenarios.
+		add_filter( 'woocommerce_composite_current_scenario_data', array( __CLASS__, 'scenario_data' ), 10, 4 );
+
+		if ( version_compare( WC_CP()->version, '8.0' ) < 0 ) {
+			// Allow 'overlay_image' scenario actions to be created via the REST API.
+			add_filter( 'woocommerce_rest_api_extended_composite_scenarios_field_args', array( __CLASS__, 'add_rest_api_scenario_action' ) );
+		}
+	}
+
+	/**
+	 * CP version check notice.
+	 */
+	public static function cp_version_check_notice() {
+	    echo '<div class="error"><p>' . sprintf( __( '<strong>Composite Products &ndash; Conditional Images</strong> requires <a href="%1$s" target="_blank">WooCommerce Composite Products</a> version <strong>%2$s</strong> or higher.', 'woocommerce-composite-products-conditional-images' ), self::$cp_url, self::$req_cp_version ) . '</p></div>';
+	}
+
+	/**
+	 * Load textdomain.
+	 *
+	 * @return void
+	 */
+	public static function localize_plugin() {
+		load_plugin_textdomain( 'woocommerce-composite-products-conditional-images', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Declare HPOS( Custom Order tables) compatibility.
+	 *
+	 */
+	public static function declare_hpos_compatibility() {
+
+		if ( ! class_exists( 'Automattic\WooCommerce\Utilities\FeaturesUtil' ) ) {
+			return;
+		}
+
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', plugin_basename( __FILE__ ), true );
+	}
+
+	/**
+	 * Front-end script.
+	 *
+	 * @param array $dependencies
+	 */
+	public static function frontend_script( $dependencies ) {
+
+		if ( ! current_theme_supports( 'wc-product-gallery-slider' ) ) {
+			return false;
+		}
+
+		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+		wp_register_script( 'wc-add-to-cart-composite-ci', self::plugin_url() . '/assets/js/single-product' . $suffix . '.js', array( 'jquery', 'underscore', 'backbone' ), self::$version, true );
+		$dependencies[] = 'wc-add-to-cart-composite-ci';
+
+		return $dependencies;
+	}
+
+	/**
+	 * Dequeue zoom script.
+	 */
+	public static function dequeue_zoom_script( $dependencies ) {
+
+		global $product;
+
+		if ( ! current_theme_supports( 'wc-product-gallery-slider' ) ) {
+			return false;
+		}
+
+		$has_overlay_image_scenarios = false;
+		$scenario_metadata           = $product->get_scenario_data();
+
+		if ( ! empty( $scenario_metadata ) ) {
+			foreach ( $scenario_metadata as $scenario_id => $metadata ) {
+				if ( isset( $metadata[ 'scenario_actions' ][ 'overlay_image' ][ 'is_active' ] ) && 'yes' === $metadata[ 'scenario_actions' ][ 'overlay_image' ][ 'is_active' ] ) {
+					$has_overlay_image_scenarios = true;
+					break;
+				}
+			}
+		}
+
+		if ( $has_overlay_image_scenarios ) {
+			wp_dequeue_script( 'zoom' );
+		}
+	}
+
+	/**
+	 * Admin scripts and styles.
+	 *
+	 * @return void
+	 */
+	public static function admin_scripts() {
+
+		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		$script_dependency = 'wc-composite-admin-product-panel';
+		wp_register_script( 'wc-cp-ci-admin-product-metaboxes', self::plugin_url() . '/assets/js/meta-boxes-product' . $suffix . '.js', array( 'jquery', 'jquery-ui-datepicker', 'wp-util', 'wc-admin-product-meta-boxes', $script_dependency ), self::$version );
+
+		$style_dependency = 'wc-composite-writepanel-css';
+		wp_register_style( 'wc-cp-ci-admin-product-metaboxes-css', self::plugin_url() . '/assets/css/meta-boxes-product.css', array( 'woocommerce_admin_styles', $style_dependency ), self::$version );
+		wp_style_add_data( 'wc-cp-ci-admin-product-metaboxes-css', 'rtl', 'replace' );
+
+		// Get admin screen id.
+		$screen    = get_current_screen();
+		$screen_id = $screen ? $screen->id : '';
+
+		if ( 'product' === $screen_id ) {
+			wp_enqueue_script( 'wc-cp-ci-admin-product-metaboxes' );
+
+			$params = array(
+				'i18n_choose_component_image' => __( 'Choose an Image', 'woocommerce-composite-products-conditional-images' ),
+				'i18n_set_component_image'    => __( 'Save Image', 'woocommerce-composite-products-conditional-images' )
+			);
+
+			wp_localize_script( 'wc-cp-ci-admin-product-metaboxes', 'wc_cp_ci_admin_params', $params );
+		}
+
+		if ( in_array( $screen_id, array( 'edit-product', 'product' ) ) ) {
+			wp_enqueue_style( 'wc-cp-ci-admin-product-metaboxes-css' );
+		}
+	}
+
+	/**
+	 * Save qty override settings in scenarios
+	 *
+	 * @param  array  $scenario_meta
+	 * @param  array  $scenario_post_data
+	 * @param  string $scenario_id
+	 * @param  array  $composite_meta
+	 * @param  string $composite_id
+	 * @return array
+	 */
+	public static function process_scenario_data( $scenario_meta, $scenario_post_data, $scenario_id, $composite_meta, $composite_id ) {
+
+		$is_active = ! empty( $scenario_post_data[ 'scenario_actions' ][ 'overlay_image' ][ 'is_active' ] );
+		$image_id  = ! empty( $scenario_post_data[ 'scenario_actions' ][ 'overlay_image' ][ 'image_id' ] ) ? $scenario_post_data[ 'scenario_actions' ][ 'overlay_image' ][ 'image_id' ] : '';
+
+		// Save defaults.
+		$scenario_meta[ 'scenario_actions' ][ 'overlay_image' ][ 'is_active' ] = 'no';
+		$scenario_meta[ 'scenario_actions' ][ 'overlay_image' ][ 'image_id' ] = '';
+
+		// Save image src.
+		if ( $image_id ) {
+			$scenario_meta[ 'scenario_actions' ][ 'overlay_image' ][ 'image_id' ] = $image_id;
+		}
+
+		// Save active state.
+		if ( $is_active && $image_id ) {
+			$scenario_meta[ 'scenario_actions' ][ 'overlay_image' ][ 'is_active' ] = 'yes';
+		}
+
+		return $scenario_meta;
+	}
+
+	/**
+	 * Add qty data in scenarios
+	 *
+	 * @param  array  $scenario_data
+	 * @param  array  $component_options
+	 * @param  string $composite
+	 * @return array
+	 */
+	public static function scenario_data( $scenario_data, $component_options, $composite ) {
+
+		if ( ! current_theme_supports( 'wc-product-gallery-slider' ) ) {
+			return $scenario_data;
+		}
+
+		$scenario_metadata = $composite->get_scenario_data();
+
+		if ( ! empty( $scenario_data[ 'scenarios' ] ) ) {
+
+			foreach ( $scenario_data[ 'scenarios' ] as $scenario_id ) {
+
+				if ( isset( $scenario_metadata[ $scenario_id ][ 'scenario_actions' ][ 'overlay_image' ][ 'is_active' ] ) && 'yes' === $scenario_metadata[ $scenario_id ][ 'scenario_actions' ][ 'overlay_image' ][ 'is_active' ] ) {
+
+					$image_id = ! empty( $scenario_metadata[ $scenario_id ][ 'scenario_actions' ][ 'overlay_image' ][ 'image_id' ] ) ? $scenario_metadata[ $scenario_id ][ 'scenario_actions' ][ 'overlay_image' ][ 'image_id' ] : '';
+
+					if ( ! $image_id ) {
+						continue;
+					}
+
+					$image_size = apply_filters( 'woocommerce_gallery_image_size', 'woocommerce_single' );
+					$image      = wp_get_attachment_image( $image_id, $image_size, false, apply_filters( 'woocommerce_gallery_image_html_attachment_image_params', array(
+						'class'    => 'wp-post-image wc-cp-overlay-image'
+					), $image_id, $image_size, true ) );
+
+					$scenario_data[ 'scenario_settings' ][ 'overlay_image' ][ $scenario_id ] = $image;
+				}
+			}
+		}
+
+		return $scenario_data;
+	}
+
+	/**
+	 * Add 'Overlay Image' action in Scenarios.
+	 *
+	 * @return void
+	 */
+	public static function scenario_admin_actions_html( $id, $scenario_data, $composite_data, $product_id ) {
+
+		$overlay_image = isset( $scenario_data[ 'scenario_actions' ][ 'overlay_image' ][ 'is_active' ] ) && 'yes' === $scenario_data[ 'scenario_actions' ][ 'overlay_image' ][ 'is_active' ] ? 'yes' : 'no';
+		$image_id      = ! empty( $scenario_data[ 'scenario_actions' ][ 'overlay_image' ][ 'image_id' ] ) ? $scenario_data[ 'scenario_actions' ][ 'overlay_image' ][ 'image_id' ] : '';
+		$image         = $image_id ? wp_get_attachment_thumb_url( $image_id ) : '';
+		$is_cp_gte_80  = version_compare( WC_CP()->version, '8.0' ) >= 0;
+
+		if ( $is_cp_gte_80 ) {
+			?>
+			<div class="scenario_action_config_group scenario_action_overlay_image_group" >
+				<div class="toggle_scenario_action_config toggle_overlay_image">
+					<label for="scenario_action_overlay_image_<?php echo $id; ?>">
+						<input id="scenario_action_overlay_image_<?php echo $id; ?>" type="checkbox" class="checkbox scenario_action_overlay_image" <?php echo ( $overlay_image === 'yes' ? ' checked="checked"' : '' ); ?> name="bto_scenario_data[<?php echo $id; ?>][scenario_actions][overlay_image][is_active]" <?php echo ( $overlay_image === 'yes' ? ' value="1"' : '' ); ?> />
+						<?php
+							echo __( 'Overlay Image', 'woocommerce-composite-products-conditional-images' );
+							echo wc_help_tip( __( 'Enable this option to conditionally overlay an image over the main Composite Product image. When using this feature, product image zooming will be disabled.', 'woocommerce-composite-products-conditional-images' ) );
+						?>
+					</label>
+				</div>
+				<div class="action_config action_conditional_images" <?php echo ( $overlay_image === 'no' ? ' style="display:none;"' : '' ); ?> >
+					<a href="#" class="upload_conditional_image_button <?php echo $image_id ? 'has_image': ''; ?>">
+						<span class="prompt"><?php echo __( 'Select image', 'woocommerce-composite-products' ); ?></span>
+						<img src="<?php if ( ! empty( $image ) ) echo esc_attr( $image ); else echo esc_attr( wc_placeholder_img_src() ); ?>" />
+						<input type="hidden" name="bto_scenario_data[<?php echo $id; ?>][scenario_actions][overlay_image][image_id]" class="image" value="<?php echo $image_id; ?>" />
+					</a>
+					<a href="#" class="remove_conditional_image_button <?php echo $image_id ? 'has_image': ''; ?>"><?php echo __( 'Remove image', 'woocommerce-composite-products' ); ?></a>
+				</div>
+			</div>
+			<?php
+
+		} else {
+
+			?>
+			<div class="scenario_action_overlay_image_group" >
+				<div class="form-field toggle_overlay_image">
+					<label for="scenario_action_overlay_image_<?php echo $id; ?>">
+						<?php echo __( 'Overlay Image', 'woocommerce-composite-products-conditional-images' ); ?>
+					</label>
+					<input id="scenario_action_overlay_image_<?php echo $id; ?>" type="checkbox" class="checkbox scenario_action_overlay_image" <?php echo ( $overlay_image === 'yes' ? ' checked="checked"' : '' ); ?> name="bto_scenario_data[<?php echo $id; ?>][scenario_actions][overlay_image][is_active]" <?php echo ( $overlay_image === 'yes' ? ' value="1"' : '' ); ?> /><?php
+						echo wc_help_tip( __( 'Enable this option to conditionally overlay an image over the main Composite Product image. When using this feature, product image zooming will be disabled.', 'woocommerce-composite-products-conditional-images' ) );
+					?>
+				</div>
+				<div class="form-field action_conditional_images" <?php echo ( $overlay_image === 'no' ? ' style="display:none;"' : '' ); ?> >
+					<a href="#" class="upload_conditional_image_button <?php echo $image_id ? 'has_image': ''; ?>">
+						<span class="prompt"><?php echo __( 'Select image', 'woocommerce-composite-products' ); ?></span>
+						<img src="<?php if ( ! empty( $image ) ) echo esc_attr( $image ); else echo esc_attr( wc_placeholder_img_src() ); ?>" />
+						<input type="hidden" name="bto_scenario_data[<?php echo $id; ?>][scenario_actions][overlay_image][image_id]" class="image" value="<?php echo $image_id; ?>" />
+					</a>
+					<a href="#" class="remove_conditional_image_button <?php echo $image_id ? 'has_image': ''; ?>"><?php echo __( 'Remove image', 'woocommerce-composite-products' ); ?></a>
+				</div>
+			</div>
+			<?php
+		}
+	}
+
+	/**
+	 * Add support for creating 'overlay_image' scenario actions via the REST API.
+	 *
+	 * @since  1.1.1
+	 * @param  array
+	 */
+	public static function add_rest_api_scenario_action( $args ) {
+		$args[ 'schema' ][ 'items' ][ 'properties' ][ 'actions' ][ 'items' ][ 'properties' ][ 'action_id' ][ 'enum' ][] = 'overlay_image';
+		return $args;
+	}
+}
+
+WC_CP_Conditional_Images::init();
---
+++ plugins/composite-products-conditional-images-for-woocommerce/readme.txt
@@ -0,0 +1,133 @@
+=== Composite Products - Conditional Images ===
+
+Contributors: franticpsyx, SomewhereWarm
+Tags: woocommerce, composite, products, conditional, image, layers, overlay
+Requires at least: 4.4
+Tested up to: 6.3
+Stable tag: 1.3.0
+Requires PHP: 5.6
+WC requires at least: 3.1
+WC tested up to: 8.0
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Free mini-extension for WooCommerce Composite Products that allows you to create dynamic, multi-layer Composite Product images that respond to option changes.
+
+
+== Description ==
+
+Free mini-extension for [WooCommerce Composite Products](https://woocommerce.com/products/composite-products/) that allows you to create dynamic, multi-layer Composite Product images that respond to option changes. The mini-extension leverages [Scenarios](https://docs.woocommerce.com/document/composite-products/composite-products-advanced-configuration/) to conditionally layer additional images over the main Composite Product image.
+
+
+== Documentation ==
+
+The plugin introduces a new "Overlay Image" Scenario Action that lets you:
+
+* Choose an image from your Media library.
+* Define **conditions** for layering this image over the main Composite Product image.
+
+This approach makes it possible to utilize either **swapping** or **compositing** techniques to conditionally modify the main product image.
+
+For more information and examples, check out the plugin's repository on [GitHub](https://github.com/somewherewarm/woocommerce-composite-products-conditional-images).
+
+**Important**: The code in this plugin is provided "as is". Support via the WordPress.org forum is provided on a **voluntary** basis only. If you have an active subscription for WooCommerce Composite Products, please be aware that WooCommerce Support may not be able to assist you with this experimental plugin.
+
+
+= Image Swapping =
+
+If you have previously worked with Variable Products and Variation Images, chances are you are already familiar with this technique. Here's the idea behind image swapping:
+
+The main product image is **swapped** with an image that depicts the chosen options as soon as the configuration of the Composite Product is complete.
+
+Instead of replacing the product images, the same effect can be achieved by covering the main product image with a fully opaque (non-transparent) image.
+
+
+= Image Compositing =
+
+**Compositing** is the combination of multiple layers of images into a single image. The final composition creates the illusion that all stacked images are parts of the same scene. For this technique to work, each layered image must include:
+
+* some **transparent** areas that allow lower layers of the stack to be seen;
+* some **opaque** areas that introduce new elements in the scene by covering the layers below; and
+* possibly, some **semi-trasparent** areas that introduce new elements while partially allowing the layers below to be seen.
+
+**Note**: When multiple **Overlay Image** Scenarios exist, each adds its own image to the main product image stack. The layering order of these conditional images depends on the relative position of their Scenarios: If Scenario A is positioned higher than Scenario B under **Product Data > Scenarios**, then the image added by Scenario A will appear higher in the stack than the image added by Scenario B.
+
+
+= Swapping vs Compositing =
+
+The **Swapping** technique is a viable choice if the number of Component Options is relatively low, as it requires you to **capture one picture** and **create one Scenario** for every possible configuration. Its main advantage over the **Compositing** technique is that it allows you to obtain a natural-looking result, even if you don't have advanced image processing skills.
+
+The **Compositing** technique is often a better choice if the number of possible configurations is high, as it requires you to create one "compositable" image for each Component Option. However, specialized knowledge and tools are required to produce a natural-looking result. For this reason, the Compositing technique is often preferred when 3D product models are available, as 3D modelling software can speed up the production of "compositable" images.
+
+
+= Limitations =
+
+1. The product image cannot be opened in a lightbox once the mini-extension has stacked additional images over it.
+2. The image zooming feature included with WooCommerce is not compatible with the swapping/compositing techniques introduced by the mini-extension. When a Composite Product includes Scenarios with the **Overlay Image** Scenario Action, the image zooming feature of the Composite Product is disabled.
+3. The plugin works with themes that declare support for the `wc-product-gallery-slider` feature.
+
+
+== Installation ==
+
+Composite Products - Conditional Images:
+
+1. Requires the official [WooCommerce Composite Products](https://woocommerce.com/products/composite-products/) extension. Before installing this plugin, please ensure that you are running the latest versions of both **WooCommerce** and **WooCommerce Composite Products**.
+2. Only works with themes that declare support for the `wc-product-gallery-slider` feature.
+
+
+== Screenshots ==
+
+1. A Composite Product with a dynamic, multi-layer product image.
+2. Using Scenarios to create conditional image layers.
+
+
+== Changelog ==
+
+= 1.3.0 =
+* Feature - Declared compatibility with the new High-Performance order tables.
+* Feature - Introduced compatibility with the new block-based Single Product Template.
+
+= 1.2.6 =
+* Fix - Fixed an issue that prevented the 'Overlay Image' action options from rendering correctly.
+
+= 1.2.5 =
+* Fix - Make sure that the order of composited images is always following the order of Scenarios.
+
+= 1.2.4 =
+* Fix - Improved Scenario Action styles when using Composite Products v8.0.
+
+= 1.2.3 =
+* Fix - Declared compatibility with the latest WooCommerce and WordPress versions.
+* Fix - Added support for Composite Products v8.0.
+
+= 1.2.2 =
+* Fix - Declared WooCommerce 4.2 compatibility.
+
+= 1.2.1 =
+* Fix - Declared WooCommerce 4.0 compatibility.
+
+= 1.2.0 =
+* Tweak - Renamed plugin to comply with WordPress.org guidelines.
+
+= 1.1.1 =
+* Fix - Added support for creating 'overlay_image' scenarios via the REST API.
+* Tweak - Declared support for WordPress 5.3 and WooCommerce 3.9.
+
+= 1.1.0 =
+* Fix - Compatibility with Composite Products 5.0+. Older versions of Composite Products are no longer supported.
+
+= 1.0.2 =
+* Fix - Resolve issue with Composite Products that do not have a product image set.
+
+= 1.0.1 =
+* Fix - Resize stacked images when the viewport width changes.
+* Tweak - Adjust overlay image width based on main 'img' width.
+
+= 1.0.0 =
+* Initial Release.
+
+
+== Upgrade Notice ==
+
+= 1.2.6 =
+Fixed an issue that prevented the 'Overlay Image' action options from rendering correctly.
