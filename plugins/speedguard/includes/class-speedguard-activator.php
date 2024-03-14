634503f51e08b52222babbce5bf38a4d0ae3176f
root <root@vultr>
2024-03-14 02:00:07 +0000

    Added plugin speedguard

---
+++ plugins/speedguard/includes/class-speedguard-activator.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://sabrinazeidan.com/
+ * @since      1.0.0
+ *
+ * @package    Speedguard
+ * @subpackage Speedguard/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Speedguard
+ * @subpackage Speedguard/includes
+ * @author     Sabrina Zeidan <sabrinazeidan@gmail.com>
+ */
+class Speedguard_Activator {
+	public static function activate() {	
+		set_transient( 'speedguard-notice-activation', true, 20 );
+		$add_homepage = SpeedGuard_Tests::try_add_speedguard_test(get_site_url());
+	}			
+			
+}	 
+
