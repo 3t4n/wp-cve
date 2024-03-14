da8c33879d4df596d7c3f9edf6fd95ea7c73b57f
root <root@vultr>
2024-03-14 11:30:35 +0000

    Added plugin media-cleaner

---
+++ plugins/media-cleaner/media-cleaner.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Media Cleaner
+Plugin URI: https://meowapps.com
+Description: Clean your WordPress! Eliminate unused and broken media files. For a faster, and better website.
+Version: 6.7.3
+Author: Jordy Meow
+Author URI: https://jordymeow.com
+Text Domain: media-cleaner
+
+Originally developed for two of my websites:
+- Jordy Meow (https://offbeatjapan.org)
+- Haikyo (https://haikyo.org)
+*/
+
+if ( !defined( 'WPMC_VERSION' ) ) {
+  define( 'WPMC_VERSION', '6.7.3' );
+  define( 'WPMC_PREFIX', 'wpmc' );
+  define( 'WPMC_DOMAIN', 'media-cleaner' );
+  define( 'WPMC_ENTRY', __FILE__ );
+  define( 'WPMC_PATH', dirname( __FILE__ ) );
+  define( 'WPMC_URL', plugin_dir_url( __FILE__ ) );
+  define( 'WPMC_ITEM_ID', 987 );
+}
+
+require_once( 'classes/init.php');
+
+?>
