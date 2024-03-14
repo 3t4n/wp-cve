bd8ab0ab63c1f1ae5274cb8bbc7372fffa29bb6e
root <root@vultr>
2024-03-14 02:59:39 +0000

    Added plugin magic-widgets

---
+++ plugins/magic-widgets/class.Unfiltered_Text_Widget.php
@@ -0,0 +1,213 @@
+<?php # -*- coding: utf-8 -*- php-version: 5.4 -*-
+
+/**
+ * Similar to the native Text widget, this class offers a plain textarea.
+ * And visibility options.
+ *
+ * @version 2013.05.28
+ */
+class Unfiltered_Text_Widget extends WP_Widget
+{
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		parent::__construct(
+			'unfiltered_text',
+			__( 'Unfiltered Text', 'plugin_magic_widgets' ),
+			array (
+				'description' => __( 'Pure Markup', 'plugin_magic_widgets' )
+			),
+			array (
+				'width'  => 300,
+				'height' => 150
+			)
+		);
+	}
+
+	/**
+	 * Front end output
+	 *
+	 * @param  array $args
+	 * @param  array $instance
+	 * @return void
+	 */
+	public function widget( $args, $instance )
+	{
+		do_action( 'tmw_before_show_widget', $instance, $args );
+
+		if ( empty ( $instance[ 'visibility' ] ) ) {
+			print $instance[ 'text' ];
+
+			return;
+		}
+
+		$user_logged_in = is_user_logged_in();
+
+		switch ( $instance[ 'visibility' ] )
+		{
+			case 'all':
+				print $instance['text'];
+				break;
+
+			case 'members':
+				print $user_logged_in ? $instance['text'] : '';
+				break;
+
+			case 'anonymous':
+				print $user_logged_in ? '' : $instance['text'];
+				break;
+
+			default: // custom visibility option
+				do_action( 'tmw_show_widget', $instance, $args );
+		}
+
+		do_action( 'tmw_after_show_widget', $instance, $args );
+	}
+
+	/**
+	 * Prepares the content
+	 *
+	 * @param  array $new_instance New content
+	 * @param  array $old_instance Old content
+	 * @return array New content
+	 */
+	public function update( $new_instance, $old_instance )
+	{
+		$visibility = $this->get_visibility_options();
+
+		if ( empty ( $new_instance[ 'visibility' ] )
+			or ! isset ( $visibility[ $new_instance[ 'visibility' ] ] )
+		)
+			$new_instance[ 'visibility' ] = $this->get_default_visibility();
+
+		$new_instance[ 'title' ] = esc_attr( $new_instance[ 'title' ] );
+
+		return $new_instance;
+	}
+
+	/**
+	 * Backend form
+	 *
+	 * @param array $instance
+	 * @return void
+	 */
+	public function form( $instance )
+	{
+		$instance = wp_parse_args(
+			$instance,
+			array(
+				'text'       => '',
+				'title'      => '',
+				'visibility' => $this->get_default_visibility()
+			)
+		);
+
+		print $this->get_title( $instance[ 'title' ] );
+		$text = format_to_edit( $instance[ 'text' ] );
+		print $this->get_textarea( $text, 'text' );
+		print $this->get_visibility_html( $instance[ 'visibility' ], 'visibility' );
+	}
+
+	/**
+	 * Add a title to identify the widget in the widget manager.
+	 *
+	 * @ticket https://github.com/toscho/WP-Magic-Widgets/issues/3
+	 * @param  string $title
+	 * @return string
+	 */
+	protected function get_title( $title )
+	{
+		$title = esc_attr( $title );
+		$id    = $this->get_field_id( 'title' );
+		$name  = $this->get_field_name( 'title' );
+		$label = __( 'Title:' );
+
+		return "<p>
+			<label for='$id'>$label
+				<input class='widefat' id='$id' name='$name' value='$title' type='text'>
+			</label>
+		</p>";
+	}
+
+	/**
+	 * Create the textarea for the main content
+	 *
+	 * @param  string $content
+	 * @param  string $name
+	 * @return string
+	 */
+	protected function get_textarea( $content, $name )
+	{
+		return sprintf(
+			'<p>
+			<textarea class="widefat code" rows="7" cols="20" id="%1$s" name="%2$s">%3$s</textarea>
+			</p>',
+			$this->get_field_id( $name ),
+			$this->get_field_name( $name ),
+			$content
+		);
+	}
+
+	/**
+	 * Render visibility radio buttons
+	 *
+	 * @param  string $current
+	 * @param  string $name
+	 * @return string
+	 */
+	protected function get_visibility_html( $current, $name )
+	{
+		$options = $this->get_visibility_options();
+		$out = '<fieldset>
+		<legend><b>' . __( 'Visibility', 'plugin_magic_widgets' ) .'</b></legend>
+		<ul>';
+
+		foreach ( $options as $key => $label )
+		{
+			$out .= sprintf(
+				'<li>
+				<label for="%1$s">
+				<input type="radio" name="%2$s" id="%1$s" value="%3$s" %4$s> %5$s
+				</label>
+				</li>',
+				$this->get_field_id( $key ),
+				$this->get_field_name( $name ),
+				$key,
+				checked( $key, $current, FALSE ),
+				esc_html( $label )
+			);
+		}
+
+		return "$out</ul></fieldset>";
+	}
+
+	/**
+	 * Default options for widget visibility
+	 *
+	 * @uses   apply_filters tmw_visibility_options
+	 * @return array
+	 */
+	protected function get_visibility_options()
+	{
+		$options = array (
+			'all'       => __( 'All', 'plugin_magic_widgets' ),
+			'members'   => __( 'Members only', 'plugin_magic_widgets' ),
+			'anonymous' => __( 'Anonymous visitors only', 'plugin_magic_widgets' )
+		);
+
+		return apply_filters( 'tmw_visibility_options', $options );
+	}
+
+	/**
+	 * Get the first visibility options key as default
+	 *
+	 * @return string
+	 */
+	protected function get_default_visibility()
+	{
+		$options = $this->get_visibility_options();
+		return key( $options );
+	}
+}
---
+++ plugins/magic-widgets/toschos-magic-widgets.php
@@ -0,0 +1,152 @@
+<?php
+/*
+ * Plugin Name: Toscho’s Magic Widgets
+ * Plugin URI:  http://toscho.de/2011/wordpress-magische-widgets/
+ * Description: Extra widgets for your HTML headers and footers.
+ * Version:     2014.01.22
+ * Author:      Thomas Scholz
+ * Author URI:  http://toscho.de
+ * License:     GPL v2
+ * Textdomain:  plugin_magic_widgets
+ * Domain Path: /languages
+*/
+
+add_action( 'widgets_init', array ( 'Toscho_Magic_Widgets', 'init' ), 20 );
+
+/**
+ * Master class.
+ * @version 1.0
+ */
+class Toscho_Magic_Widgets
+{
+	/**
+	 * Actions and names for the new Widgets.
+	 *
+	 * @type array
+	 */
+	protected $actions = array ();
+
+	/**
+	 * Prefix for the widget IDs.
+	 * Filled by the constructor.
+	 *
+	 * @type string
+	 */
+	protected $prefix = '';
+
+	/**
+	 * Handler for the action 'widgets_init'. Instantiates this class.
+	 *
+	 * @uses   apply_filters tmw_class
+	 * @return void
+	 */
+	public static function init()
+	{
+		// If want to use another class (an extension maybe),
+		// change the class name here.
+		$class = apply_filters( 'tmw_class', __CLASS__ );
+
+		// Named global variable to make access for other scripts easier.
+		if ( empty ( $GLOBALS[ $class ] ) )
+		{
+			$GLOBALS[ $class ] = new $class;
+		}
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * Registers the Unfiltered Text widget.
+	 */
+	public function __construct()
+	{
+		/** @noinspection PhpUnusedLocalVariableInspection */
+		$dummy = __( 'Extra widgets for your HTML headers and footers.', 'plugin_magic_widgets' );
+		// Uppercase letters don’t work.
+		$this->prefix = strtolower( __CLASS__ ) . '_';
+
+		if ( is_admin() )
+			$this->load_language();
+
+		// The extra widget.
+		$widget_class = 'Unfiltered_Text_Widget';
+
+		if ( ! class_exists( $widget_class ) )
+			require_once plugin_dir_path( __FILE__ ) . "class.$widget_class.php";
+
+		register_widget( $widget_class );
+
+		$this->sidebar_actions();
+	}
+
+	/**
+	 * Set up sidebars and add the print_widget action
+	 *
+	 * @uses   add_action()
+	 * @return void
+	 */
+	public function sidebar_actions()
+	{
+		$this->actions = array (
+			'wp_head'       => __( 'Front End Header', 'plugin_magic_widgets' ),
+			'wp_footer'     => __( 'Front End Footer', 'plugin_magic_widgets' ),
+			'admin_head'    => __( 'Back End Header', 'plugin_magic_widgets' ),
+			'admin_footer'  => __( 'Back End Footer', 'plugin_magic_widgets' )
+		);
+
+		// You may add or remove actions here.
+		// Use add_filter( 'magic_widgets_actions', 'your custom_filter', 10, 1 );
+		$this->actions = apply_filters( 'magic_widgets_actions', $this->actions );
+
+		// Register the areas and additional actions.
+		foreach ( $this->actions as $action => $name )
+		{
+			register_sidebar(
+				array (
+					'name'          => $name,
+					'id'            => $this->prefix . $action,
+					'description'   => __( 'Use the Unfiltered Text widget.', 'plugin_magic_widgets' ),
+				// Erase all other output
+					'before_widget' => '',
+					'after_widget'  => '',
+					'before_title'  => '',
+					'after_title'   => ''
+				)
+			);
+
+			add_action( $action, array ( $this, 'print_widget' ) );
+		}
+	}
+
+	/**
+	 * Output
+	 *
+	 * @return boolean
+	 */
+	public function print_widget()
+	{
+		// current_filter() is the name of the action.
+		return dynamic_sidebar( $this->prefix . current_filter() );
+	}
+
+	/**
+	 * Loads translation file.
+	 *
+	 * @return bool
+	 */
+	public function load_language()
+	{
+		$path = plugin_basename( dirname( __FILE__ ) ) . '/languages';
+		return load_plugin_textdomain( 'plugin_magic_widgets', FALSE, $path );
+	}
+
+	/**
+	 * Remove translations from memory.
+	 *
+	 * @return void
+	 */
+	public function unload_language()
+	{
+		unset ( $GLOBALS['l10n']['plugin_magic_widgets'] );
+	}
+}
