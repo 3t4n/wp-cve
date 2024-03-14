e073fafafd459440e1fe1d82351ed40e779515ec
root <root@vultr>
2024-03-14 22:54:56 +0000

    Added plugin global-meta-box-order

---
+++ plugins/global-meta-box-order/Readme.txt
@@ -0,0 +1,197 @@
+=== Global Meta Box Order ===
+Contributors: nosurs
+Tags:  admin, custom, customize, customization, post, page, custom-post-type, dashboard, meta, meta-box, metabox, ui
+Requires at least: 4.1
+Tested up to: 4.3
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Harmonize meta box positions for all backend users.
+
+== Description == 
+
+Intuitively harmonize meta box positions and screen column layout for all backend users of your WordPress installation.  
+
+= Quick Overview =
+
+* Install and activate the plugin
+* Switch to a post, a page, a custom post type, or the dashboard
+* Change the order and position of a meta box (or all of them)
+* Change the column layout
+* Switch to a different user (maybe with the help of the [*User Switching*](https://wordpress.org/plugins/user-switching/) plugin)
+* See your changes applied   
+
+= Installation =
+
+Download and unpack, then move the folder 'global-meta-box-order' into your 'plugins' folder. Head over to your WordPress installation and activate the plugin in the admin area.
+
+= Rollback =
+
+The plugin doesn't write anything to the database, it just reads. So it never touches any user settings, but instead filters them on a per request basis. Though all applied changes *appear* to be permanent from a user's perspective, they are not. Just deactivate the plugin and see all changes disappear. Activate it again, and they will all be reapplied.  
+
+= How It Works =
+
+The plugin operates on a blueprint user whose screen settings for meta boxes (visibility, position and ordering), and column layout are cloned for all other backend users on the fly.
+
+By default, this blueprint user is the first admin user found, so you'll need to be logged in as **that user** to globally change screen settings. For how to change the default blueprint user as well as other settings, please refer to the *Configuration* section below.
+
+= Where It Works  =
+
+By default, the plugin kicks in when a user:
+
+* edits a post
+* edits a page
+* edits a custom post type
+* hits the dashboard
+
+= What It Changes =
+
+It will always change
+
+* the meta boxes visibility, ordering, and column positions
+* the column layout
+
+When told so, it will also
+
+* remove the screen options box
+* immobilize all boxes, so they can't be moved around by your users anymore
+
+= Usage =
+
+Log in as your blueprint user. By default, the is the first admin user found in your system.   
+
+Select an editing screen (post, page, custom post type) or the dashboard, move the meta boxes around, change their screen settings and the screen's column layout. Switch to some user to review your settings, switch back to adjust them.
+
+When done, you might want to lock your views down by removing the screen options box and by locking the boxes' sort order. Your blueprint user, of course, wil not be affected by this. 
+
+= Configuration =
+
+The backend integration is kept to a minimum. No navigation entry, no options page, no entry in the database. Instead, the place to go to configure the plugin is your theme's *functions.php*.
+
+By the way: You don't *need* to configure the plugin. As long as it finds an admin user, it will work just fine.
+
+**Preparation**
+
+Fire up an editor, load your *functions.php*, and copy and paste the following code into it. The idea is to have some sort of container to do the configuration in, but do it any way you like.
+
+For brevity, we'll assume the plugin is loaded and active, so we won't check for that (see this nice [write-up](http://queryloop.com/how-to-detect-if-a-wordpress-plugin-is-active/) on *QueryLoop* on some ways to do it, though).
+
+`
+if (is_admin()) {
+
+    // The path to the configuation is rather long, so let's
+    // make us a shorthand.
+    class_alias('\GlobalMetaBoxOrder\Config', 'MetaBoxConfig');
+
+    // Add MetaBoxConfig below this line
+    ...
+}
+`
+
+Now we are ready to add some of the configuration settings below to adjust the plugin's default behaviour. You might also want to have a look at the example configuration near the end of this document.
+
+Please keep in mind that you need to be logged in as any user but your blueprint user to see a setting applied. Again, the [*User Switching*](https://wordpress.org/plugins/user-switching/) plugin might come in handy.
+
+**Screens To Operate On**
+
+By default, the plugin operates on the post, page, and custom post type editing screens, and the dashboard.
+
+You can change this as follows:
+
+`
+// Operate on post and page screens only, leave the dashboard alone.
+// This will still include custom post types.
+MetaBoxConfig::$filter = array('post', 'page');
+
+// Exclude custom post types
+MetaBoxConfig::$include_cpts = false; 
+
+// Allow custom post types...
+MetaBoxConfig::$include_cpts = true; 
+
+// ...but not all of them
+MetaBoxConfig::$exclude = array('acme_product');
+`
+
+*MetaBoxConfig* in the example above is assumed to be an alias to *\GlobalMetaBoxOrder\Config* as shown in the preparation section above.    
+
+**Changing the Blueprint User**
+
+Register a function that returns a user id, like so:
+
+`
+MetaBoxConfig::$getBlueprintUserId = function () { return 1; };
+`
+
+Or, more involved:
+
+`
+MetaBoxConfig::$getBlueprintUserId = function () { 
+            
+    $user = get_user_by('slug', 'jane');
+    return $user ? $user->ID : false; 
+};
+`
+
+*MetaBoxConfig* in the example above is assumed to be an alias to *\GlobalMetaBoxOrder\Config* as shown in the preparation section above.        
+
+**Locking Views**
+
+By default, all users will be able to interact with the screen options box, and to move around the meta boxes themselves. There is a rationale behind it, but to cut things short, this is how you might want to change it:
+
+`
+// No screen options 
+MetaBoxConfig::$remove_screen_options = true;
+
+// Meta boxes can't be moved anymore 
+MetaBoxConfig::$lock_meta_box_order = true; 
+`
+
+*MetaBoxConfig* in the example above is assumed to be an alias to *\GlobalMetaBoxOrder\Config* as shown in the preparation section above.    
+
+**Example Configuration**
+`
+if (is_admin()) {
+
+    // Make sure plugin is active
+    if (class_exists('\GlobalMetaBoxOrder\Config')) {
+
+        // Make a long name short. 
+        class_alias('\GlobalMetaBoxOrder\Config', 'MetaBoxConfig');
+
+        // Settings
+
+        MetaBoxConfig::$filter = array('post', 'page', 'dashboard'); // default
+        MetaBoxConfig::$include_cpts = true; // default
+        MetaBoxConfig::$getBlueprintUserId = function () { return 1; };
+        MetaBoxConfig::$exclude = array('acme_product');
+        MetaBoxConfig::$remove_screen_options = true;
+        MetaBoxConfig::$lock_meta_box_order = true; 
+    }
+}
+`
+
+= On Moving the WYSIWYG Editor =
+
+The position of WordPress' WYSIWYG editor is fixed, and can't be changed out of the box (mostly because it lacks a box around it). There are reasons for this, but if you want to have a positionable editor anyway, you might want to have a look at our very own [*Movable Editor*](https://github.com/pontycode/wordpress-movable-editor) plugin.
+
+If, on the other hand, if you want to place one specific box *above* the editor, you might want to check out [this answer](http://wordpress.stackexchange.com/a/88103) on *stackexchange*.
+
+== Screenshots ==
+
+1. A post editing screen with all sorts of meta boxes turned on. A somewhat cluttered view, potentially distracting your editors, and overwhelming new users.  
+
+2. An uncluttered view, leaving only those meta boxes turned on your users might need for their work. Also, the order of the meta boxes has changed a bit to improve your editors workflow.  
+
+3. The new uncluttered view gets applied to all of your users, so they only see the meta boxes they need to see, in the order you define. 
+
+== Changelog ==
+
+= 1.0.3 =
+* Check for AJAX requests
+
+== Upgrade Notice ==
+
+= 1.0.3 =
+Fixed a bug with incoming AJAX requests.
---
+++ plugins/global-meta-box-order/index.php
@@ -0,0 +1,452 @@
+<?php
+
+/*
+
+Plugin Name: Global Meta Box Order
+Description: Harmonize column layout and meta box positions across all backend users of your WordPress installation.
+Version: 1.0.3
+Plugin URI: https://github.com/pontycode/wordpress-custom-metabox-order/
+Author: nosurs
+Author URI: https://github.com/pontycode
+License: GPL v2 or later
+
+Copyright © 2013-2015 Pontycode
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+*/
+
+namespace GlobalMetaBoxOrder;
+
+define(__NAMESPACE__.'\VERSION', '1.0.3');
+
+/**
+ * Configuration
+ *
+ * Example
+ * -------
+ *
+ * To replace the default function for getting the blueprint user id,
+ * add the following to your theme's function.php:
+ *
+ * \GlobalMetaBoxOrder\Config::$getBlueprintUserId = function () { return $someUserId; };
+ *
+ */
+class Config {
+
+    /**
+     * On which post & screen types to operate on.
+     *
+     * @var Array
+     */
+    public static $filter = array('post', 'page', 'dashboard');
+
+    /**
+     * Whether or not to include Custom Post Types.
+     *
+     * @var Boolean
+     */
+    public static $include_cpts = true;
+
+    /**
+     * Post types to exclude
+     *
+     * Use this if you want to include Custom Post Types,
+     * but exclude some of them.
+     *
+     * @var Array
+     */
+    public static $exclude = array();
+
+    /**
+     * Remove screen options for selected screens
+     *
+     * @var Boolean
+     */
+    public static $remove_screen_options = false;
+
+    /**
+     * Lock meta box order. When switched on, your users
+     * won't be able to move the boxes around anymore.
+     *
+     * @var Boolean
+     */
+    public static $lock_meta_box_order = false;
+
+    /**
+     * Register a function here returning
+     * a valid user id.
+     *
+     * See below for default implementation.
+     *
+     * @var String
+     */
+    public static $getBlueprintUserId;
+}
+
+/**
+ * Initialize an editor
+ *
+ * @return Integer - User Id
+ */
+Config::$getBlueprintUserId = function () {
+
+    $clone_from = get_users(array('role' => 'administrator'));
+
+    if (!empty($clone_from)) {
+
+        return $clone_from[0]->ID;
+    }
+};
+
+/**
+ * Clone meta box order on the fly, i.e., without
+ * actually changing any user settings.
+ *
+ * Originally based on work & ideas by:
+ *
+ * http://gist.github.com/franz-josef-kaiser/9100450
+ * http://wordpress.stackexchange.com/a/144608
+ * http://wordpress.stackexchange.com/a/19972
+ */
+class MetaBoxOrder {
+
+    /**
+     * User to clone from
+     *
+     * @var Int
+     */
+    protected $clone_from_user_id;
+
+    /**
+     * Screens to clone in
+     *
+     * @var Array
+     */
+    protected $allowed_screens = array();
+
+    /**
+     * Meta box keys to clone
+     *
+     * @var Array
+     */
+    protected $clone_meta_keys = array();
+
+    /**
+     * To prevent an endless loop, we need to be able
+     * to remove and re-apply {@see cloneMeta()}.
+     *
+     * @return void
+     */
+    protected function removeFilter() {
+
+        remove_filter('get_user_metadata', array($this, 'cloneMeta'));
+    }
+
+    /**
+     * To prevent an endless loop, we need to be able
+     * to remove and re-apply {@see cloneMeta()}.
+     *
+     * @return void
+     */
+    protected function addFilter() {
+
+        // http://developer.wordpress.org/reference/hooks/get_meta_type_metadata/
+        add_filter('get_user_metadata', array($this, 'cloneMeta'), 10, 3);
+    }
+
+    /**
+     * What base screen we are on, if any.
+     *
+     * @return WP_Screen|false
+     */
+    protected function getCurrentScreen() {
+
+        if (defined('DOING_AJAX') && DOING_AJAX) {
+
+            return false;
+        }
+
+        $screen = get_current_screen();
+
+        if (!($screen && property_exists($screen, 'base'))) {
+
+            return false;
+        }
+
+        return $screen;
+    }
+
+    /**
+     * Whether the given screen is in the list of allowed ones.
+     *
+     * @param  WP_Screen
+     * @return Boolean
+     */
+    protected function isScreenAllowed(\WP_Screen $screen) {
+
+        // Be as specific as it gets
+        $screen = $screen->post_type ? $screen->post_type : $screen->base;
+        return in_array($screen, $this->allowed_screens);
+    }
+
+    /**
+     * Whether the given user is the blueprint user.
+     *
+     * @param  Int
+     * @return Boolean
+     */
+    protected function isBlueprintUser($user_id) {
+
+        return $this->clone_from_user_id === $user_id;
+    }
+
+    /**
+     * Normalize a meta key, which might or might not
+     * be prefixed.
+     *
+     * @param  String
+     * @return String - a key without its prefix
+     */
+    protected function normalizeMetaKey($meta_key) {
+
+        global $wpdb;
+
+        if (strrpos($meta_key, $wpdb->prefix) === 0) {
+
+            $meta_key = substr($meta_key, strlen($wpdb->prefix));
+        }
+
+        return $meta_key;
+    }
+
+    /**
+     * Clone meta box order (technically, this will clone any key
+     * registered by {@see setMetaKeys()}).
+     *
+     * @return void
+     */
+    public function cloneMeta($abort, $user_id, $meta_key) {
+
+        // Return early on wrong user or screen
+
+        if ($this->isBlueprintUser($user_id)) {
+
+           return $abort;
+        }
+
+        $screen = $this->getCurrentScreen();
+
+        if (!$screen) {
+
+            return $abort;
+        }
+
+        if (!$this->isScreenAllowed($screen)) {
+
+            $this->removeFilter();
+            return $abort;
+        }
+
+        // Clone
+
+        $meta_key = $this->normalizeMetaKey($meta_key);
+
+        if (!in_array($meta_key, $this->clone_meta_keys)) {
+
+            return $abort;
+        }
+
+        // Remove ourselves...
+
+        $this->removeFilter();
+
+        // ...because we don't want to get caught in an endless loop while cloning
+
+        $cloned = get_user_meta(
+
+            $this->clone_from_user_id,
+            $meta_key,
+            true
+        );
+
+        if (is_array($cloned)) {
+
+            $cloned = array($cloned); // wp-includes/meta.php::get_metadata() will ask for a $check[0] (~448ff.);
+                                      // since this is what becomes $check, make sure it has [0].
+        }
+
+        // WP might call for the same user data dozens of times. Make sure it keeps us posted again.
+
+        $this->addFilter();
+
+        // The easy part. Return the clone.
+
+        return $cloned;
+    }
+
+    /**
+     * Clone colum layout.
+     *
+     * @return void
+     */
+    protected function cloneColumnLayout() {
+
+        $screens = array_unique(array_map(function ($meta_key) {
+
+            return substr($meta_key, strpos($meta_key, '_'));
+
+        }, $this->clone_meta_keys));
+
+        foreach ($screens as $screen) {
+
+            $clone = get_user_option('screen_layout'.$screen, $this->clone_from_user_id);
+
+            add_filter('get_user_option_screen_layout'.$screen, function ($columns) use ($clone) {
+
+                return $clone ? $clone : $columns;
+
+            }, 10, 1);
+        }
+    }
+
+    /**
+     * Remove screen options for selected screens.
+     *
+     * @return void
+     */
+    protected function removeScreenOptions() {
+
+        add_filter('screen_options_show_screen', function () {
+
+            return !$this->isScreenAllowed($this->getCurrentScreen());
+        });
+    }
+
+    /**
+     * Remove screen options for selected screens.
+     *
+     * @return void
+     */
+    protected function lockMetaBoxOrder() {
+
+        add_action('admin_enqueue_scripts', function () {
+
+            $screen = $this->getCurrentScreen();
+
+            if (!$this->isScreenAllowed($screen)) {
+
+                return;
+            }
+
+            wp_enqueue_script(
+
+                'global_meta_box_order',
+                plugin_dir_url(__FILE__).'lock_order.js',
+                array('jquery', 'jquery-ui-sortable'),
+                \GlobalMetaBoxOrder\VERSION,
+                true
+            );
+        });
+    }
+
+    /**
+     * Apply configuration
+     *
+     * @return void
+     */
+    protected function setup() {
+
+        $screens  = Config::$filter;
+
+        if (Config::$include_cpts) {
+
+            $cpts    = get_post_types(array('_builtin' => false));
+            $screens = array_merge($screens, $cpts);
+        }
+
+        foreach (Config::$exclude as $type) {
+
+            $exclude = array_search($type, $screens);
+
+            if ($exclude !== false) {
+
+                unset($screens[$exclude]);
+            }
+        }
+
+        foreach ($screens as $type) {
+
+            $this->clone_meta_keys[] = 'meta-box-order_'.$type;
+            $this->clone_meta_keys[] = 'metaboxhidden_'.$type;
+        }
+
+        $this->allowed_screens = $screens;
+    }
+
+    /**
+     * Init actions.
+     *
+     * @return void
+     */
+    protected function init() {
+
+        $this->addFilter(); // Main sorting action
+        $this->cloneColumnLayout(); // Not configurable by design.
+                                    // As to the why: Make a one column layout, switch to
+                                    // a user with a two column layout, _don't_ clone, and
+                                    // see everything stuffed in the wrong column.
+
+        if (Config::$remove_screen_options) {
+
+            $this->removeScreenOptions();
+        }
+
+        if (Config::$lock_meta_box_order) {
+
+            $this->lockMetaBoxOrder();
+        }
+    }
+
+    /**
+     * Constructor.
+     *
+     * Proceeds to setup and further action only if
+     * the current is not the blueprint user.
+     */
+    public function __construct() {
+
+        $getId = Config::$getBlueprintUserId;
+
+        if (is_callable($getId)) {
+
+            $id      = $getId();
+            $exists  = get_user_by('id', $id);
+            $current = get_current_user_id();
+
+            if ($id && $exists && $id != $current) {
+
+                $this->clone_from_user_id = $id;
+
+                $this->setup();
+                $this->init();
+            }
+        }
+    }
+}
+
+if (is_admin()) {
+
+    add_action('wp_loaded', function () {
+
+        new MetaBoxOrder();
+    });
+}
