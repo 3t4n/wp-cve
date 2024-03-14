01ccc95ea5a77a0d7f00720d6706153c4d99dde1
root <root@vultr>
2024-03-14 02:00:50 +0000

    Added plugin login-logout-register-menu

---
+++ plugins/login-logout-register-menu/includes/class-login-logout-register-menu-activator.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * 
+ * @since      1.0.0
+ * @package    LLRM
+ * @subpackage LLRM/includes
+ * @author     Vinod Dalvi <mozillavvd@gmail.com>
+ */
+class Login_Logout_Register_Menu_Activator {
+
+	/**
+	 * Short Description.
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
