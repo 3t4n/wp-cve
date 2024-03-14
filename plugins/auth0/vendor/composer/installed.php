6eda85379d9d044a437f0cb9c43607fd10296d7e
root <root@vultr>
2024-03-14 20:43:32 +0000

    Added plugin auth0

---
+++ plugins/auth0/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'auth0/wp-auth0',
+        'pretty_version' => '4.x-dev',
+        'version' => '4.9999999.9999999.9999999-dev',
+        'reference' => '257c59d0d5dc74c2f8e6afea0a52dd533373a713',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'auth0/php-jwt' => array(
+            'pretty_version' => '3.3.4',
+            'version' => '3.3.4.0',
+            'reference' => 'a0daa1a728cf85230843ebb8c1183047fe493284',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../auth0/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'auth0/wp-auth0' => array(
+            'pretty_version' => '4.x-dev',
+            'version' => '4.9999999.9999999.9999999-dev',
+            'reference' => '257c59d0d5dc74c2f8e6afea0a52dd533373a713',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
