b6a2d872632bf6394682c20232bb08431738b65f
root <root@vultr>
2024-03-14 02:00:00 +0000

    Added plugin classic-menu-block

---
+++ plugins/classic-menu-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'e732e900117e7807e56a');
