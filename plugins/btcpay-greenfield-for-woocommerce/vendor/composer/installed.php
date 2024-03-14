e8acd5470e6532bcca1346e3239464dccc98e301
root <root@vultr>
2024-03-14 16:56:44 +0000

    Added plugin btcpay-greenfield-for-woocommerce

---
+++ plugins/btcpay-greenfield-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'btcpayserver/btcpay-greenfield-for-woocommerce',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'btcpayserver/btcpay-greenfield-for-woocommerce' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'btcpayserver/btcpayserver-greenfield-php' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '9a01503ce9f395bdbb9326fd380650c8faf449f6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../btcpayserver/btcpayserver-greenfield-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
