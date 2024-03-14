2150e67c7a5211e9d618c00a14a4e2953f59f49a
root <root@vultr>
2024-03-14 11:30:43 +0000

    Added plugin wp-special-textboxes

---
+++ plugins/wp-special-textboxes/stb-db-tools.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: minimus
+ * Date: 14.09.2019
+ * Time: 9:12
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+require_once 'stb-default-themes.php';
+
+if (!class_exists('StbDbTools')) {
+    class StbDbTools
+    {
+        private string $sTable = '';
+        private string $osTable = '';
+        private array $defaultColors;
+        private array $defaultImages;
+
+        public function __construct($sTable = 'stb_stylez', $osTable = 'stb_styles')
+        {
+            global $wpdb;
+
+            $stbDefaultThemes = new StbDefaultThemes();
+
+            $this->sTable = $wpdb->prefix . $sTable;
+            $this->osTable = $wpdb->prefix . $osTable;
+            $this->defaultColors = $stbDefaultThemes->getTheme('stb-dark')['styles'];
+            $this->defaultImages = $stbDefaultThemes->getThemeDefaultImages('stb-dark');
+        }
+
+        protected function isSetValue($valid, $invalid)
+        {
+            if (isset($valid)) return $valid;
+            return $invalid;
+        }
+
+        public function tableExists($table): bool
+        {
+            global $wpdb;
+
+            return $wpdb->get_var("SHOW TABLES LIKE '$table'") == $table;
+        }
+
+        public function createStylesTable()
+        {
+            global $wpdb;
+            $charset = $wpdb->get_charset_collate();
+
+            $sql = "CREATE TABLE $this->sTable (
+                 slug varchar(55) NOT NULL,
+                 caption varchar(255) NOT NULL,
+                 colors text DEFAULT NULL,
+                 stbType varchar(8) DEFAULT NULL,
+                 trash tinyint(1) DEFAULT 0,
+                 PRIMARY KEY (slug)
+                ) $charset;";
+            dbDelta($sql);
+        }
+
+        public function convertStyles(): array
+        {
+            global $wpdb;
+
+            $getSql = "SELECT slug, caption, css_style, stype, trash FROM $this->osTable;";
+            $rows = $wpdb->get_results($getSql, ARRAY_A);
+            $styles = [];
+            foreach ($rows as $row) {
+                $cssStyles = unserialize($row['css_style']);
+                $style = [
+                    'slug' => $row['slug'],
+                    'caption' => $row['caption'],
+                    'colors' => serialize([
+                        'body' => [
+                            'color' => '#' . $cssStyles['color'],
+                            'background' => array('#' . $cssStyles['bgColor'], '#' . $cssStyles['bgColorEnd']),
+                        ],
+                        'border' => [
+                            'color' => '#' . $cssStyles['borderColor'],
+                        ],
+                        'caption' => [
+                            'color' => '#' . $cssStyles['captionColor'],
+                            'background' => array('#' . $cssStyles['captionBgColor'], '#' . $cssStyles['captionBgColorEnd']),
+                        ],
+                        'image' => [
+                            'image' => strpos($cssStyles['image'], STB_URL) === 0 ? '' : $cssStyles['image'],
+                            'defaultImage' => $this->defaultImages[$row['slug']],
+                            'enabled' => !(strpos($cssStyles['image'], STB_URL) === 0),
+                        ],
+                    ]),
+                    'stbType' => $row['stype'],
+                    'trash' => (int)$row['trash'],
+                ];
+                $styles[] = $style;
+                $wpdb->insert($this->sTable, $style, ['%s', '%s', '%s', '%s', '%d']);
+            }
+            return $styles;
+        }
+
+        private function insertDefaultStyles(): void
+        {
+            global $wpdb;
+
+            foreach ($this->defaultColors as $row) {
+                $wpdb->insert($this->sTable, $row, ['%s', '%s', '%s', '%s', '%d']);
+            }
+        }
+
+        public function updateStyles($styles): void
+        {
+            global $wpdb;
+
+            foreach ($styles as $style) {
+                $wpdb->replace($this->sTable, $style, ['%s', '%s', '%s', '%s', '%d']);
+            }
+        }
+
+        public function upgradeDb(): bool
+        {
+            if (!self::tableExists($this->sTable)) {
+                self::createStylesTable();
+                if (self::tableExists($this->osTable)) {
+                    self::convertStyles();
+                } else {
+                    self::insertDefaultStyles();
+                }
+            }
+
+            return self::tableExists($this->sTable);
+        }
+
+        public function getCurrentColors($filter = ''): array
+        {
+            global $wpdb;
+
+            $sTable = $wpdb->prefix . "stb_stylez";
+            $sSql = "SELECT * FROM $sTable st{$filter}";
+            $colors = $wpdb->get_results($sSql, ARRAY_A);
+            $data = [];
+            foreach ($colors as $color) {
+                $data[] = [
+                    'slug' => $color['slug'],
+                    'type' => $color['stbType'],
+                    'caption' => $color['caption'],
+                    'colors' => unserialize($color['colors']),
+                    'trash' => (int)$color['trash']
+                ];
+            }
+
+            return $data;
+        }
+
+        public function getColor(string $slug): array
+        {
+            global $wpdb;
+            $sTable = $wpdb->prefix . "stb_stylez";
+            $sSql = $wpdb->prepare("SELECT * FROM $sTable st WHERE st.slug = %s;", [$slug]);
+            $colorSet = $wpdb->get_row($sSql, ARRAY_A);
+
+            return $colorSet
+                ? [
+                    'slug' => $colorSet['slug'],
+                    'type' => $colorSet['stbType'],
+                    'caption' => $colorSet['caption'],
+                    'colors' => unserialize($colorSet['colors']),
+                    'trash' => (int)$colorSet['trash']
+                ]
+                : [];
+        }
+
+        public function getColorsNames(): array {
+            global $wpdb;
+            $sTable = $wpdb->prefix . "stb_stylez";
+            $sSql = "SELECT st.slug FROM $sTable st;";
+            return $wpdb->get_col($sSql);
+        }
+
+        public function deleteTables(bool $all = true): void
+        {
+            global $wpdb;
+
+            $wpdb->query("DROP TABLE IF EXISTS {$this->sTable};");
+            if ($all) {
+                $wpdb->query("DROP TABLE IF EXISTS {$this->osTable};");
+            }
+            delete_option('stb_db_version');
+        }
+    }
+}
---
+++ plugins/wp-special-textboxes/stb-rest-api.php
@@ -0,0 +1,371 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+include_once 'stb-tools.php';
+include_once 'stb-db-tools.php';
+
+const routeNamespace = 'stb/v6';
+
+if (!class_exists('StbRestApi')) {
+    class StbRestApi
+    {
+        public object $currentUser;
+        public object $stbTools;
+        public object $stbDbTools;
+
+        public function __construct($currentUser)
+        {
+            $this->currentUser = $currentUser;
+            $this->stbTools = new StbTools();
+            $this->stbDbTools = new StbDbTools();
+        }
+
+        public function setAdminRoutes(): void
+        {
+            // ** SETTINGS ROUTE START **
+            register_rest_route(routeNamespace, '/admin/settings', [
+                [
+                    'methods' => WP_REST_Server::READABLE,
+                    'callback' => [$this, 'getSettings'],
+                    'permission_callback' => [$this, 'adminAccess'],
+                ], [
+                    'methods' => WP_REST_Server::EDITABLE,
+                    'callback' => [$this, 'setSettings'],
+                    'permission_callback' => [$this, 'adminAccess'],
+                ]
+            ]);
+            // ** SETTINGS ROUTE END **
+
+            // ** STYLES ROUTE START **
+            register_rest_route(routeNamespace, '/admin/styles/(?P<filter>\d+)', [
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => [$this, 'getStyles'],
+                'permission_callback' => [$this, 'adminAccess'],
+                'args' => [
+                    'filter' => [
+                        'type' => 'integer',
+                        'required' => true,
+                        'validate_callback' => function ($param, $request, $key) {
+                            return is_integer((int)$param) && (int)$param >= 0 && (int)$param <= 2;
+                        }
+                    ],
+                ],
+            ]);
+            // ** STYLES ROUTE END **
+
+            // ** COLORS ROUTE START **
+            register_rest_route(routeNamespace, '/admin/colors/(?P<slug>\S+)', [
+                [
+                    'methods' => WP_REST_Server::READABLE,
+                    'callback' => [$this, 'getColors'],
+                    'permission_callback' => [$this, 'adminAccess'],
+                    'args' => [
+                        'slug' => [
+                            'type' => 'string',
+                            'required' => true,
+                            'validate_callback' => function ($param, $request, $key) {
+                                return self::validateColorNames($param, $request, $key);
+                            }
+                        ],
+                    ],
+                ], [
+                    'methods' => WP_REST_Server::EDITABLE,
+                    'callback' => [$this, 'setColors'],
+                    'permission_callback' => [$this, 'adminAccess'],
+                    'args' => [
+                        'slug' => [
+                            'type' => 'string',
+                            'required' => true,
+                            'validate_callback' => function ($param, $request, $key) {
+                                return preg_match('/^[\da-zA-Z\-_]+$/', $param) === 1;
+                            }
+                        ],
+                    ],
+                ], [
+                    'methods' => WP_REST_Server::DELETABLE,
+                    'callback' => [$this, 'deleteColors'],
+                    'permission_callback' => [$this, 'adminAccess'],
+                    'args' => [
+                        'slug' => [
+                            'type' => 'string',
+                            'required' => true,
+                            'validate_callback' => function ($param, $request, $key) {
+                                return self::validateColorNames($param, $request, $key);
+                            }
+                        ],
+                    ],
+                ]
+            ]);
+            // ** COLORS ROUTE END **
+
+            // ** THEMES ROUTE START **
+            register_rest_route(routeNamespace, '/admin/themes', [
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => [$this, 'getThemesInfo'],
+                'permission_callback' => [$this, 'adminAccess'],
+            ]);
+
+            register_rest_route('stb/v6', '/admin/themes/(?P<slug>\S+)', [
+                'methods' => WP_REST_Server::EDITABLE,
+                'callback' => [$this, 'activateTheme'],
+                'permission_callback' => [$this, 'adminAccess'],
+                'args' => [
+                    'slug' => [
+                        'type' => 'string',
+                        'required' => true,
+                        'validate_callback' => function ($param, $request, $key) {
+                            return self::validateThemeName($param, $request, $key);
+                        }
+                    ],
+                ],
+            ]);
+            // ** THEMES ROUTE END **
+
+            // ** SYSINFO ROUTE START **
+            register_rest_route(routeNamespace, '/admin/sysinfo', [
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => [$this, 'getSysInfo'],
+                'permission_callback' => [$this, 'adminAccess'],
+            ]);
+            // ** SYSINFO ROUTE END **
+
+            // ** LOCALIZATION ROUTE START **
+            register_rest_route('stb/v6', '/admin/locale', [
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => [$this, 'getLocaleStrings'],
+                'permission_callback' => [$this, 'adminAccess'],
+            ]);
+            // ** LOCALIZATION ROUTE END **
+
+            // ** CLIENT SIDE THEME REQUEST START **
+            register_rest_route(routeNamespace, '/theme/(?P<slug>\S+)', [
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => [$this, 'getTheme'],
+                'permission_callback' => '__return_true',
+                'args' => [
+                    'slug' => [
+                        'type' => 'string',
+                        'required' => true,
+                        'validate_callback' => function ($param, $request, $key) {
+                            return self::validateThemeName($param, $request, $key);
+                        }
+                    ],
+                ],
+            ]);
+            // ** CLIENT SIDE THEME REQUEST END **
+        }
+
+        private function getCurrentSettings(): array
+        {
+            return get_option('SpecialTextBoxesAdminSettings', false);
+        }
+
+        public function adminAccess($request): bool
+        {
+            return current_user_can('manage_options');
+        }
+
+        public function validateThemeName($param, $request, $key): bool
+        {
+            include_once 'stb-default-themes.php';
+            $defaultThemes = new StbDefaultThemes();
+            $themes = $defaultThemes->getThemesNames();
+            return in_array($param, $themes);
+        }
+
+        public function validateColorNames($param, $request, $key): bool
+        {
+            $names = $this->stbDbTools->getColorsNames();
+            return in_array($param, $names);
+        }
+
+        public function getSettings($data): array
+        {
+            $settings = $this->stbTools->getSettings();
+            if (!$settings) {
+                $options = get_option('SpecialTextBoxesAdminOptions', false);
+                if (!$options) {
+                    require_once 'stb-default-themes.php';
+                    $stbDefaultThemes = new StbDefaultThemes();
+                    $settings = $stbDefaultThemes->getTheme('stb-dark')['settings'];
+                } else {
+                    $settings = $this->stbTools->convertOptions($options);
+                }
+            }
+            return ['settings' => $settings];
+        }
+
+        public function setSettings($request): array
+        {
+            include_once 'stb-styles.php';
+            $settings = $request->get_json_params();
+            if (!$settings) {
+                return ['success' => false];
+            } else {
+                update_option('SpecialTextBoxesAdminSettings', $settings, true);
+                $colors = $this->stbDbTools->getCurrentColors();
+                $stbStyles = new StbStyles($settings, $colors);
+                $written = $stbStyles->writeStyles(false, true, true);
+                return ['success' => true, 'data' => $settings, 'written' => $written];
+            }
+        }
+
+        public function getStyles($request): array
+        {
+            $filter = (string)$request['filter'];
+            $sqlFilter = '';
+            if ($filter != '0') {
+                $sqlFilter = ($filter == '2') ? ' WHERE st.trash = 1' : ' WHERE st.trash = 0';
+            }
+            $data = $this->stbDbTools->getCurrentColors($sqlFilter);
+            return ['data' => $data];
+        }
+
+        public function getColors($request): array
+        {
+            $slug = (string)$request['slug'];
+            $data = $this->stbDbTools->getColor($slug);
+
+            return count($data)
+                ? ['result' => 'ok', 'data' => $data]
+                : ['result' => 'error'];
+        }
+
+        public function setColors($request): array
+        {
+            include_once 'stb-styles.php';
+
+            global $wpdb;
+
+            $sTable = $wpdb->prefix . "stb_stylez";
+
+            $slug = (string)$request['slug'];
+            $colorSet = $request->get_json_params();
+
+            $result = $wpdb->replace($sTable, [
+                'slug' => $slug,
+                'stbType' => $colorSet['type'],
+                'caption' => $colorSet['caption'],
+                'colors' => serialize($colorSet['colors']),
+                'trash' => (int)$colorSet['trash']
+            ], ['%s', '%s', '%s', '%s', '%d']);
+
+            if (!$result && $result !== 0) {
+                return ['result' => 'error'];
+            } else {
+                $stbDbTools = new StbDbTools();
+                $settings = self::getCurrentSettings();
+                $colors = $stbDbTools->getCurrentColors();
+                $stbStyles = new StbStyles($settings, $colors);
+                $written = $stbStyles->writeStyles(false, true, true);
+                return ['result' => 'ok', 'completed' => $result, 'written' => $written];
+            }
+        }
+
+        public function deleteColors($request): array
+        {
+            global $wpdb;
+            $sTable = $wpdb->prefix . "stb_stylez";
+
+            $slug = (string)$request['slug'];
+
+            $result = $wpdb->delete($sTable, ['slug' => $slug], '%s');
+
+            if (!$result && $result !== 0) {
+                return ['result' => 'error'];
+            } else {
+                return ['result' => 'ok', 'deleted' => $result];
+            }
+        }
+
+        public function getThemesInfo(): array
+        {
+            include_once 'stb-default-themes.php';
+
+            $themesModel = new StbDefaultThemes();
+            return ['data' => $themesModel->getThemesInfo()];
+        }
+
+        private function getThemeProps(string $slug): array
+        {
+            include_once 'stb-default-themes.php';
+            $themeModel = new StbDefaultThemes();
+
+            ['styles' => $themeStyles, 'settings' => $themeSettings] = $themeModel->getTheme($slug);
+            $colors = [];
+
+            foreach ($themeStyles as $style) {
+                $colors[] = [
+                    'slug' => $style['slug'],
+                    'type' => $style['stbType'],
+                    'caption' => $style['caption'],
+                    'colors' => unserialize($style['colors']),
+                    'trash' => (int)$style['trash']
+                ];
+            }
+
+            return [
+                'themeStyles' => $themeStyles,
+                'colors' => $colors,
+                'settings' => $themeSettings
+            ];
+        }
+
+        public function activateTheme($request): array
+        {
+            include_once 'stb-styles.php';
+
+            $slug = (string)$request['slug'];
+
+            [
+                'themeStyles' => $themeStyles,
+                'settings' => $themeSettings,
+                'colors' => $colors
+            ] = self::getThemeProps($slug);
+
+            $stbStyles = new StbStyles($themeSettings, $colors);
+            $written = $stbStyles->writeStyles(false, true, true);
+
+            $this->stbDbTools->updateStyles($themeStyles);
+            update_option('SpecialTextBoxesAdminSettings', $themeSettings, true);
+
+            return ['result' => 'ok', 'data' => $slug, 'written' => $written];
+        }
+
+        public function getTheme($request): array
+        {
+            $slug = (string)$request['slug'];
+            ['colors' => $colors, 'settings' => $themeSettings] = self::getThemeProps($slug);
+
+            return ['result' => 'ok', 'data' => ['colors' => $colors, 'settings' => $themeSettings]];
+        }
+
+        public function getSysInfo(): array
+        {
+            global $wpdb;
+
+            $row = $wpdb->get_row('SELECT VERSION() AS ver', ARRAY_A);
+            $sqlVersion = $row['ver'];
+            $mem = ini_get('memory_limit');
+            return ['data' => [
+                'version' => STB_VERSION,
+                'dbVersion' => STB_DB_VERSION,
+                'phpVersion' => PHP_VERSION,
+                'sqlVersion' => $sqlVersion,
+                'memoryLimit' => $mem,
+                'wpMemoryLimit' => WP_MEMORY_LIMIT,
+                'wpMemoryLimitMax' => WP_MAX_MEMORY_LIMIT,
+            ]];
+        }
+
+        public function getLocaleStrings(): array
+        {
+            require_once 'stb-locales.php';
+            $localizer = new StbLocales();
+
+            return ['result' => 'ok', 'data' => $localizer->getLocales()];
+        }
+    }
+}
