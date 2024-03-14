6b55713214138236712291ccc082ebf4bd6c81f4
root <root@vultr>
2024-03-14 10:34:42 +0000

    Added plugin woocommerce-product-addon

---
+++ plugins/woocommerce-product-addon/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/woocommerce-product-addon',
+        'pretty_version' => 'v32.0.13',
+        'version' => '32.0.13.0',
+        'reference' => '70b17c662a413e115f797772ad35f8676adbe341',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.14',
+            'version' => '3.3.14.0',
+            'reference' => '662952078c57b12e4d3af9bc98ef847ea3500206',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/woocommerce-product-addon' => array(
+            'pretty_version' => 'v32.0.13',
+            'version' => '32.0.13.0',
+            'reference' => '70b17c662a413e115f797772ad35f8676adbe341',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
