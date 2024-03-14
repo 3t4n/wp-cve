18981956ee1671c067cf8e9d0951ab91a0fe6c04
root <root@vultr>
2024-03-14 08:41:02 +0000

    Added plugin syntax-highlighting-code-block

---
+++ plugins/syntax-highlighting-code-block/build/customize-controls.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch'), 'version' => '69165f056abb568c8401');
---
+++ plugins/syntax-highlighting-code-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'd85d2f596d4538c3479f');
