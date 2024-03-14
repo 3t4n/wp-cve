3bb293a3c6d3a9a8f050dd673cff33b508a74564
root <root@vultr>
2024-03-14 11:07:45 +0000

    Added plugin bangladeshi-payment-gateways

---
+++ plugins/bangladeshi-payment-gateways/includes/Helper.php
@@ -0,0 +1,96 @@
+<?php // @codingStandardsIgnoreLine
+/**
+ * Helper Class
+ *
+ * @package BDPaymentGateways
+ * @since 1.0.0
+ */
+
+namespace ultraDevs\BDPG;
+
+/**
+ * Helper Class
+ *
+ * @package BDPaymentGateways
+ * @since 1.0.0
+ */
+class Helper {
+
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {}
+
+	/**
+	 * Add Option
+	 *
+	 * @param string $key Option Key.
+	 * @param mixed  $value Option Value.
+	 */
+	public static function add_option( $key, $value ) {
+		// Get Option.
+		add_option( $key, $value );
+	}
+
+	/**
+	 * Get Option
+	 *
+	 * @param string $key Option Key.
+	 * @param mixed  $default Option Default.
+	 */
+	public static function get_option( $key, $default = false ) {
+		// Get Option.
+		return get_option( $key, $default );
+	}
+
+	/**
+	 * Save Option
+	 *
+	 * @param string $key Option Key.
+	 * @param mixed  $value Option Value.
+	 */
+	public static function update_option( $key, $value ) {
+		// Get Option.
+		update_option( $key, $value );
+	}
+
+	/**
+	 * Time to Day(s) Converter
+	 *
+	 * @param int $time Time.
+	 * @return int
+	 */
+	public static function time_to_days( $time ) {
+
+		$current_time = current_time( 'timestamp' ); //phpcs:ignore
+		return round( ( $current_time - $time ) / 24 / 60 / 60 );
+	}
+
+
+	/**
+	 * Get All Meta values by meta key.
+	 *
+	 * @param string $meta_key Meta Key.
+	 * @param string $post_type Post Type.
+	 * @return array
+	 */
+	public static function get_meta_values( $meta_key, $post_type = 'post' ) {
+
+		$posts = get_posts(
+			array(
+				'post_type'      => $post_type,
+				'meta_key'       => $meta_key,
+				'posts_per_page' => -1,
+			)
+		);
+
+		$meta_values = array();
+		foreach( $posts as $post ) {
+			$meta_values[] = get_post_meta( $post->ID, $meta_key, true );
+		}
+
+		return $meta_values;
+
+	}
+}
---
+++ plugins/bangladeshi-payment-gateways/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'ultradevs/bangladeshi-payment-gateways',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'appsero/client' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '985363ac0bd17d8cfaa45b0e6780ad68ccfe3ca8',
+            'dev_requirement' => false,
+        ),
+        'ultradevs/bangladeshi-payment-gateways' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
