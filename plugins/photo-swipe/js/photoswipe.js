8da3b27e16661d7a77a040dd94d9c5a9fb59d7b5
root <root@vultr>
2024-03-14 11:13:05 +0000

    Added plugin photo-swipe

---
+++ plugins/photo-swipe/js/photoswipe.js
@@ -0,0 +1,82 @@
+jQuery(function($) {
+	var PhotoSwipe = window.PhotoSwipe,
+		PhotoSwipeUI_Default = window.PhotoSwipeUI_Default;
+
+	$('body').on('click', 'a[data-size]', function(e) {
+		if( !PhotoSwipe || !PhotoSwipeUI_Default ) {
+			return;
+		}
+
+		e.preventDefault();
+		openPhotoSwipe( this );
+	});
+
+	var parseThumbnailElements = function(gallery, el) {
+		var elements = $(gallery).find('a[data-size]').has('img'),
+			galleryItems = [],
+			index;
+
+		elements.each(function(i) {
+			var $el = $(this),
+				size = $el.data('size').split('x'),
+				caption;
+
+			if( $el.next().is('.wp-caption-text') ) {
+				// image with caption
+				caption = $el.next().text();
+			} else if( $el.parent().next().is('.wp-caption-text') ) {
+				// gallery icon with caption
+				caption = $el.parent().next().text();
+			} else {
+				caption = $el.attr('title');
+			}
+
+			galleryItems.push({
+				src: $el.attr('href'),
+				w: parseInt(size[0], 10),
+				h: parseInt(size[1], 10),
+				title: caption,
+				msrc: $el.find('img').attr('src'),
+				el: $el
+			});
+			if( el === $el.get(0) ) {
+				index = i;
+			}
+		});
+
+		return [galleryItems, parseInt(index, 10)];
+	};
+
+	var openPhotoSwipe = function( element, disableAnimation ) {
+		var pswpElement = $('.pswp').get(0),
+			galleryElement = $(element).parents('.gallery, .hentry, .main, body').first(),
+			gallery,
+			options,
+			items, index;
+
+		items = parseThumbnailElements(galleryElement, element);
+		index = items[1];
+		items = items[0];
+
+		options = {
+			index: index,
+			getThumbBoundsFn: function(index) {
+				var image = items[index].el.find('img'),
+					offset = image.offset();
+
+				return {x:offset.left, y:offset.top, w:image.width()};
+			},
+			showHideOpacity: true,
+			history: false
+		};
+
+		if(disableAnimation) {
+			options.showAnimationDuration = 0;
+		}
+
+		// Pass data to PhotoSwipe and initialize it
+		gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+		gallery.init();
+	};
+
+});
---
+++ plugins/photo-swipe/readme.txt
@@ -0,0 +1,43 @@
+=== PhotoSwipe ===
+Contributors: louyx
+Tags: attachments, images, gallery, lightbox, fancybox, photoswipe
+Requires at least: 4.0
+Tested up to: 4.4.0
+Stable tag: 4.1.1.1
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A very light implementation of PhotoSwipe javascript plugin for WordPress
+
+== Description ==
+This plugins adds the PhotoSwipe library to your WordPress blog seamlessly. No configuration required.
+
+== Installation ==
+Just as any other plugin. Install from `wp-admin`.
+
+== Frequently Asked Questions ==
+= It doesn't work with some of my photos, why? =
+The PhotoSwipe library requires some special markup to be applied on images. This markup is applied with this plugin as soon as the post is saved. So if it doesn't work on images in some old post try to save that post again. It should work.
+
+== Screenshots ==
+1. PhotoSwipe Website
+
+== Changelog ==
+Same as the original PhotoSwipe. Except for the following.
+
+= 4.1.1.1 =
+* Photoswipe now works on archives as well, not just singular ones.
+* Fixed a bug in loading default-skin.css
+
+= 4.0.7.4 =
+* Show captions in WordPress galleries.
+
+= 4.0.7.3 =
+* Now showing WordPress captions when available.
+
+= 4.0.7.2 =
+* Fixed another bug in multisite & KSES.
+* Added jQuery as dependency.
+
+= 4.0.7.1 =
+* Fixed a bug in multisite.
