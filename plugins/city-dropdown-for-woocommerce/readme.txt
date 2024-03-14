5df015194bff3116a54f6a073d7d2f97805139b7
root <root@vultr>
2024-03-14 17:13:11 +0000

    Added plugin city-dropdown-for-woocommerce

---
+++ plugins/city-dropdown-for-woocommerce/readme.txt
@@ -0,0 +1,35 @@
+
+=== City Dropdown For Woocommerce ===
+Contributors: danielflorea
+Tags: woocommerce, states, cities, local government, dropdown, city, select, dropdown, city select, cities select, city dropdown, cities dropdown, woocommerce city, woocommerce cities, Romania, orase Romania
+Requires at least: 4.0
+Tested up to: 5.4
+Stable tag: 1.0.3
+WC requires at least: 3.6
+WC tested up to: 3.8.1
+License: GPLv2 or later
+License URI: [http://www.gnu.org/licenses/gpl-2.0.html](http://www.gnu.org/licenses/gpl-2.0.html)
+
+This Plugin change Woocommerce City input into a dropdown, based on states. Works only with Romania country!
+
+== Description ==
+This plugin is using places-select.js from [WC City Select](https://tw.wordpress.org/plugins/wc-city-select/)
+
+This WooCommerce plugin transforms the text input for city. With this plugin you can provide a list of cities to be shown as a select dropdown, depends on selected state.
+
+This will be shown in checkout pages, edit addresses pages, shipping calculator, etc.
+
+= Supported Countries =
+ * Romania
+
+== Installation ==
+
+= Minimum Requirements =
+
+WordPress 5.0  or greater
+Woocommerce 3.8 or greater
+PHP version 7.2 or greater
+MySQL version 5.0 or greater
+
+== Changelog ==
+*1.0.3 Added Sectors for Bucharest 
---
+++ plugins/city-dropdown-for-woocommerce/states/RO.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Romanian states
+ *
+ * @author  BetterWoo | * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states['RO'] = array(
+      					"B"  => "Bucuresti",
+				        "AB" => "Alba",
+				        "AR" => "Arad",
+				        "AG" => "Arges",
+				        "BC" => "Bacau",
+				        "BH" => "Bihor",
+				        "BN" => "Bistrita-Nasaud",
+				        "BT" => "Botosani",
+				        "BR" => "Braila",
+				        "BV" => "Brasov",
+				        "BZ" => "Buzau",
+				        "CL" => "Calarasi",
+				        "CS" => "Caras-Severin",
+				        "CJ" => "Cluj",
+				        "CT" => "Constanta", 
+				        "CV" => "Covasna",
+				        "DB" => "Dambovita",
+				        "DJ" => "Dolj",
+				        "GL" => "Galati",
+				        "GJ" => "Gorj", 
+				        "GR" => "Giurgiu",
+				        "HR" => "Harghita",
+				        "HD" => "Hunedoara",
+				        "IL" => "Ialomita", 
+				        "IS" => "Iasi",
+				        "IF" => "Ilfov",
+				        "MM" => "Maramures",
+				        "MH" => "Mehedinti",
+				        "MS" => "Mures",
+				        "NT" => "Neamt",
+				        "OT" => "Olt", 
+				        "PH" => "Prahova",
+				        "SJ" => "Salaj", 
+				        "SM" => "Satu Mare",
+				        "SB" => "Sibiu",
+				        "SV" => "Suceava",
+				        "TR" => "Teleorman",
+				        "TM" => "Timis", 
+				        "TL" => "Tulcea", 
+				        "VS" => "Vaslui", 
+				        "VL" => "Valcea", 
+				        "VN" => "Vrancea"
+				       );
+  return $states;
