b49e58d05ebe7622dfaeb892b9ab0c160827d6e8
root <root@vultr>
2024-03-14 11:01:15 +0000

    Added plugin price-table-block

---
+++ plugins/price-table-block/price-table-block.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Plugin Name:     Price Table Block
+ * Plugin URI:         https://essential-blocks.com
+ * Description:     Instantly create beautiful pricing menu for eCommerce website
+ * Version:         1.2.5
+ * Author:          WPDeveloper
+ * Author URI:         https://wpdeveloper.net
+ * License:         GPL-3.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:     price-table-block
+ *
+ * @package         price-table-block
+ */
+
+/**
+ * Registers all block assets so that they can be enqueued through the block editor
+ * in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/applying-styles-with-stylesheets/
+ */
+
+require_once __DIR__ . '/includes/font-loader.php';
+require_once __DIR__ . '/includes/post-meta.php';
+require_once __DIR__ . '/includes/helpers.php';
+require_once __DIR__ . '/lib/style-handler/style-handler.php';
+
+function create_block_pricing_table_block_init() {
+
+    define( 'PRICE_TABLE_BLOCKS_VERSION', "1.2.5" );
+    define( 'PRICE_TABLE_BLOCKS_ADMIN_URL', plugin_dir_url( __FILE__ ) );
+    define( 'PRICE_TABLE_BLOCKS_ADMIN_PATH', dirname( __FILE__ ) );
+
+    $script_asset_path = PRICE_TABLE_BLOCKS_ADMIN_PATH . "/dist/index.asset.php";
+    if ( ! file_exists( $script_asset_path ) ) {
+        throw new Error(
+            'You need to run `npm start` or `npm run build` for the "price-table-block/pricing-table" block first.'
+        );
+    }
+
+    $index_js         = PRICE_TABLE_BLOCKS_ADMIN_URL . 'dist/index.js';
+    $script_asset     = require $script_asset_path;
+    $all_dependencies = array_merge( $script_asset['dependencies'], [
+        'wp-blocks',
+        'wp-i18n',
+        'wp-element',
+        'wp-block-editor',
+        'eb-price-table-blocks-controls-util',
+        'essential-blocks-eb-animation'
+    ] );
+
+    wp_register_script(
+        'eb-pricing-table-block-editor',
+        $index_js,
+        $all_dependencies,
+        $script_asset['version']
+    );
+
+    $load_animation_js = PRICE_TABLE_BLOCKS_ADMIN_URL . 'assets/js/eb-animation-load.js';
+    wp_register_script(
+        'essential-blocks-eb-animation',
+        $load_animation_js,
+        [],
+        PRICE_TABLE_BLOCKS_VERSION,
+        true
+    );
+
+    $animate_css = PRICE_TABLE_BLOCKS_ADMIN_URL . 'assets/css/animate.min.css';
+    wp_register_style(
+        'essential-blocks-animation',
+        $animate_css,
+        [],
+        PRICE_TABLE_BLOCKS_VERSION
+    );
+
+    $fontpicker_theme = 'assets/css/fonticonpicker.base-theme.react.css';
+    wp_register_style(
+        'fontpicker-default-theme',
+        plugins_url( $fontpicker_theme, __FILE__ ),
+        []
+    );
+
+    $fontpicker_material_theme = 'assets/css/fonticonpicker.material-theme.react.css';
+    wp_register_style(
+        'fontpicker-matetial-theme',
+        plugins_url( $fontpicker_material_theme, __FILE__ ),
+        []
+    );
+
+    $editor_css = 'dist/style.css';
+    wp_register_style(
+        'eb-pricing-table-block-editor-style',
+        plugins_url( $editor_css, __FILE__ ),
+        ['fontawesome-frontend-css', 'fontpicker-default-theme', 'fontpicker-matetial-theme'],
+        filemtime( PRICE_TABLE_BLOCKS_ADMIN_PATH . "/$editor_css" )
+    );
+
+    $fontawesome_css = 'assets/css/font-awesome5.css';
+    wp_register_style(
+        'fontawesome-frontend-css',
+        plugins_url( $fontawesome_css, __FILE__ ),
+        []
+    );
+
+    $style_css = PRICE_TABLE_BLOCKS_ADMIN_URL . 'dist/style.css';
+    wp_register_style(
+        'create-block-pricing-table-block',
+        $style_css,
+        ['fontawesome-frontend-css', 'essential-blocks-animation'],
+        filemtime( PRICE_TABLE_BLOCKS_ADMIN_PATH . '/dist/style.css' )
+    );
+
+    if ( ! WP_Block_Type_Registry::get_instance()->is_registered( 'essential-blocks/pricing-table' ) ) {
+        register_block_type(
+            Price_Table_Helper::get_block_register_path( 'price-table-block/pricing-table', PRICE_TABLE_BLOCKS_ADMIN_PATH ),
+            [
+                'editor_script'   => 'eb-pricing-table-block-editor',
+                'editor_style'    => 'eb-pricing-table-block-editor-style',
+                'render_callback' => function ( $attributes, $content ) {
+                    if ( ! is_admin() ) {
+                        wp_enqueue_script( 'essential-blocks-eb-animation' );
+                        wp_enqueue_style( 'create-block-pricing-table-block' );
+                    }
+                    return $content;
+                }
+            ]
+        );
+    }
+}
+add_action( 'init', 'create_block_pricing_table_block_init' );
---
+++ plugins/price-table-block/readme.txt
@@ -0,0 +1,115 @@
+=== Price Table Block ===
+Contributors: wpdevteam, re_enter_rupok, Asif2BD, rahat89, fencermonir
+Tags: block, blocks, price, pricing, discount, offers, price table, pricing table, gutenberg, gutenberg blocks
+Requires at least: 5.6
+Tested up to: 6.2
+Stable tag: 1.2.5
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Instantly create beautiful pricing menu for eCommerce website.
+
+== Description ==
+
+With this **Pricing Table** block, you can instantly create a beautiful pricing menu for your eCommerce website within Gutenberg Editor.
+
+Without any coding or hassle, it’ll take a few clicks to create a pricing table within Gutenberg block editor using this.
+
+*Completely Customizable:* You can customize Pricing Table based on your product or service categories.
+Super Light-weight: No extra resources used and it's highly optimized for super fast loading & instant Live editing.
+
+*Dedicated Support:* We’ve an extraordinary support team ready to help you. Ask your questions in the support forum, we’ll get back to you immediately.
+
+
+## 🚀 BACKED BY A TRUSTED TEAM ##
+
+This Block is brought to you by [WPDeveloper](https://wpdeveloper.com), the team behind the most popular elements library [Essential Addons for Elementor](https://wordpress.org/plugins/essential-addons-for-elementor-lite/) (used by almost 1 million+ happy users from 180+ countries).
+
+## 👨‍💻 DOCUMENTATION AND SUPPORT ##
+
+- For documentation and tutorials go to our [documentation](https://essential-blocks.com/docs/)
+- To report issue and contribution visit [Github](https://github.com/EssentialBlocks/pricing-table).
+- If you have any more questions, visit our support on the [Plugin's Forum](https://wordpress.org/support/plugin/price-table-block).
+- For more information about features, FAQs and documentation, check out our website at [WPDeveloper](https://wpdeveloper.com/).
+
+## 🙌 JOIN THE COMMUNITY ##
+
+🎦 Subscribe to our [Youtube Channel](https://www.youtube.com/wpdevelopernet/?sub_confirmation=1) to watch video tutorials on WordPress.
+- 🤝 Join our [Facebook Community](https://www.facebook.com/groups/wpdeveloper.net/) & get connected with expert web creators like yourself.
+
+🧡 LOVED PRICING TABLE BLOCK? Rate us on [WordPress Repository](https://wordpress.org/support/plugin/pricing-table-block/reviews/#new-post)
+
+== Installation ==
+
+= Modern Way: =
+1. Go to the WordPress Block/Gutenberg Editor
+2. Search For "Pricing Table".
+3. Install in 1-click
+
+= Old Way: =
+1. Upload `price-table-block` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Follow the [Documentation](https://essential-blocks.com/docs/)
+
+
+== Frequently Asked Questions ==
+
+= Does it work with any WordPress theme? =
+
+Yes, it will work with any standard WordPress theme.
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.2.5 - 23/03/2023 =
+* Improved: Font loader & controls
+
+= 1.2.4 - 09/01/2023 =
+* Fixed: issue of enqueue css files
+
+= 1.2.3 - 12/12/2022 =
+* Fixed: Minor bugs
+* Improved: Controls and structure
+
+= 1.2.2 - 12/09/2022 =
+* Added: Pricing table block with new options and preset
+* Added: Open new tab option for button link
+* Improved: Controls and structure
+
+= 1.2.1 - 02/06/2022 =
+* Added: Responsive Controls, Animation Controls, Custom CSS
+* Improved: Controls and structure
+* Fixed: Show block when using Gutenberg Block
+
+= 1.2.0 - 09/02/2022 =
+* Added: Full site editing support
+* Improved: Controls and structure
+* Improved: Responsive device preview options synced with the WordPress's default preview button.
+
+= 1.1.3 - 23/08/2021 =
+* Improved: Security for asset generation
+* Fixed: Warning in controls
+
+== 1.1.2 - 28/07/2021 =
+* Added: Alignment Option For Every Section
+* Improved: HTML Structure For CSS Issues
+
+== 1.1.1 - 13/07/2021 =
+* Fixed: naming convention on block.json file
+* Fixed: remove extra block.json file
+
+== 1.1.0 - 11/07/2021 =
+* Added: Preset Designs, More flexibility on controls
+* Added: Dynamic asset generation for inline styles
+* Added: Responsive control
+* Improved: Inspector panel design
+
+= 1.0.1 =
+* Fix: Missing css, js files
+
+= 1.0.0 =
+* Initial Release
+
+== Upgrade Notice ==
