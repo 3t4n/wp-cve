d6f01a24024ed8f3a659eaae9e39814e42268a7a
root <root@vultr>
2024-03-14 01:02:50 +0000

    Added plugin force-pclzip

---
+++ plugins/force-pclzip/force-pclzip.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Force using PclZip
+ * Plugin URI: https://github.com/aaronjorbin/force-pclzip
+ * Description:  Bypass the use of ZipArchive and force your site to use PclZip.
+ * Version: 0.1.1
+ * Author: Aaron Jorbin
+ * Author URI: https://aaron.jorb.in
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Tested up to: 6.4.3
+ */
+
+ // if this file is called directly, abort.
+ if ( ! defined( 'WPINC' ) ) {
+	 die;
+ }
+
+ add_filter( 'unzip_file_use_ziparchive', '__return_false' );
+
