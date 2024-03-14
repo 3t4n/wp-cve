1deed9b4bef54794c6b156db6a35936fc8e43521
root <root@vultr>
2024-03-14 02:38:44 +0000

    Added plugin disable-gutenberg-autosave

---
+++ plugins/disable-gutenberg-autosave/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '5406904a5d51ac2e41c6');
