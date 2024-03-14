46655d58a8b4fb7da0f65e21bd71516445b967b4
root <root@vultr>
2024-03-14 14:01:46 +0000

    Added plugin api-key-for-google-maps

---
+++ plugins/api-key-for-google-maps/api-key-for-google-maps.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * This is the main GeoDirectory plugin file, here we declare and call the important stuff
+ *
+ * @package     GMAPIKEY
+ * @copyright   2016 AyeCode Ltd
+ * @license     GPL-2.0+
+ * @since       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name: API KEY for Google Maps
+ * Plugin URI: http://wpgeodirectory.com/
+ * Description: Adds API KEY to Google maps calls if they have been enqueue correctly.
+ * Version: 1.2.10
+ * Author: AyeCode Ltd
+ * Author URI: https://wpgeodirectory.com
+ * Text Domain: gmaps-api-key
+ * Domain Path: /languages
+ * Requires at least: 5.0
+ * Tested up to: 6.4
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The current version number.
+ *
+ * @since 1.0.0
+ */
+define( "GMAPIKEY_VERSION", "1.2.10" );
+
+
+add_action( 'plugins_loaded', 'rgmk_load_textdomain' );
+/**
+ * Load plugin textdomain.
+ *
+ * @since 1.0.0
+ */
+function rgmk_load_textdomain() {
+	load_plugin_textdomain( 'gmaps-api-key', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+}
+
+/**
+ * Clean url.
+ *
+ * @param string $url Url.
+ * @param string $original_url Original url.
+ * @param string $_context Context.
+ *
+ * @return string Modified url.
+ * @since   1.0.0
+ * @package GMAPIKEY
+ *
+ */
+function rgmk_find_add_key( $url, $original_url, $_context ) {
+	$key = get_option( 'rgmk_google_map_api_key' );
+
+	// If no key added no point in checking
+	if ( ! $key ) {
+		return $url;
+	}
+
+	// Check Google Maps API Url.
+	if ( strstr( $url, "maps.google.com/maps/api/js" ) !== false || strstr( $url, "maps.googleapis.com/maps/api/js" ) !== false ) {
+		if ( strstr( $url, "key=" ) === false ) {
+			// Key not exists
+			$url = str_replace( "&#038;", "&amp;", $url );
+			$url = add_query_arg( 'key', esc_attr( $key ), $url );
+			$url = str_replace( "&key=", "&amp;key=", $url );
+		} else {
+			// Key exists
+			if ( strstr( $url, "key=" . $key ) === false ) {
+				$url = str_replace( array( "&#038;", "&amp;key=" ), array( "&amp;", "&key=" ), $url );
+				$url = remove_query_arg( 'key', $url );
+				$url = add_query_arg( 'key', esc_attr( $key ), $url );
+				$url = str_replace( "&key=", "&amp;key=", $url );
+			}
+		}
+
+		// Since January 2023 Google made callback as a required parameter.
+		if ( strstr( $url, "?callback=" ) === false && strstr( $url, "&callback=" ) === false && strstr( $url, ";callback=" ) === false ) {
+			$url = str_replace( "&#038;", "&amp;", $url );
+			$url = add_query_arg( 'callback', 'rgmkInitGoogleMaps', $url );
+			$url = str_replace( "&callback=", "&amp;callback=", $url );
+		}
+	}
+
+	return $url;
+}
+add_filter( 'clean_url', 'rgmk_find_add_key', 99, 3 );
+
+/**
+ * Add the admin menu link.
+ *
+ * @since   1.0.0
+ * @package GMAPIKEY
+ */
+function rgmk_add_admin_menu() {
+	add_submenu_page( 'options-general.php', 'Google API KEY', 'Google API KEY', 'manage_options', 'gmaps-api-key', 'rgmk_add_admin_menu_html' );
+}
+add_action( 'admin_menu', 'rgmk_add_admin_menu' );
+
+/**
+ * The html output for the settings page.
+ *
+ * @since   1.0.0
+ * @since   1.1.0 Added button to generate API KEY from wp-admin.
+ * @package GMAPIKEY
+ */
+function rgmk_add_admin_menu_html() {
+
+	$updated = false;
+
+	if ( isset( $_POST['rgmk_google_map_api_key'] ) && ! empty( $_POST['rgmk_nonce'] ) && wp_verify_nonce( $_POST['rgmk_nonce'], 'rgmk_save' ) && current_user_can( 'manage_options' ) ) {
+		$key     = sanitize_text_field( $_POST['rgmk_google_map_api_key'] );
+		$updated = update_option( 'rgmk_google_map_api_key', $key );
+	}
+
+	if ( $updated ) {
+		echo '<div class="updated fade"><p><strong>' . __( 'Key Updated!', 'gmaps-api-key' ) . '</strong></p></div>';
+	}
+
+	$gm_api_url = 'https://console.cloud.google.com/apis/enableflow?apiid=maps_backend,static_maps_backend,street_view_image_backend,maps_embed_backend,places_backend,geocoding_backend,directions_backend,distance_matrix_backend,geolocation,elevation_backend,timezone_backend&keyType=CLIENT_SIDE&reusekey=true&pli=1';
+	?>
+    <div class="wrap">
+        <h2><?php _e( 'Retro Add Google Maps API KEY', 'gmaps-api-key' ); ?></h2>
+        <p><?php _e( 'This plugin will attempt to add your Google API KEY to any Google Maps JS file that has properly been enqueued.', 'gmaps-api-key' ); ?></p>
+        <p><a id="gd-api-key" onclick='window.open("<?php echo wp_slash( $gm_api_url ); ?>", "newwindow", "width=600, height=400"); return false;' href='<?php echo $gm_api_url; ?>' class="button-primary" name="<?php _e( 'Generate API Key - ( MUST be logged in to your Google account )', 'gmaps-api-key' ); ?>"><?php _e( 'Generate API Key', 'gmaps-api-key' ); ?></a> <?php echo __( '( MUST be logged in to your Google account )', 'gmaps-api-key' ); ?></p>
+
+        <form method="post" action="options-general.php?page=gmaps-api-key">
+            <label for="rgmk_google_map_api_key"><?php _e( 'Enter Google Maps API KEY', 'gmaps-api-key' ); ?></label>
+            <input title="<?php _e( 'Add Google Maps API KEY', 'gmaps-api-key' ); ?>" type="text" name="rgmk_google_map_api_key" id="rgmk_google_map_api_key" placeholder="<?php _e( 'Enter your API KEY here', 'gmaps-api-key' ); ?>" style="padding: 6px; width:50%; display: block;" value="<?php echo esc_attr( wp_unslash( get_option( 'rgmk_google_map_api_key' ) ) ); ?>"/>
+			<?php
+			wp_nonce_field( 'rgmk_save', 'rgmk_nonce' );
+
+			submit_button();
+			?>
+        </form>
+
+    </div><!-- /.wrap -->
+
+    <div class="">
+        <hr/>
+        <br>
+        <a target="_blank" href="https://mapfix.dev/" class="button button-primary button-hero"><?php _e( 'Check for API key errors', 'gmaps-api-key' ); ?> <span class="dashicons dashicons-external" style="line-height: 2;"></span></a>
+    </div>
+	<?php
+}
+
+/**
+ * Add special offer banner on settings page.
+ *
+ * @since   1.1.0
+ * @package GMAPIKEY
+ */
+function rgmk_show_geodirectory_offer() {
+	if ( isset( $_REQUEST['page'] ) && $_REQUEST['page'] == 'gmaps-api-key' ) {
+
+		if ( defined( 'GEODIRECTORY_VERSION' ) ) {
+			// do nothing
+		} else {
+			?>
+            <div class="notice notice-info is-dismissible rgmk-offer-notice">
+                <img src="<?php echo plugin_dir_url( __FILE__ ) . '/gd_banner.jpg'; ?>"/>
+                <p><?php echo sprintf( __( 'API KEY for Google Maps was created for free by %sGeoDirectory%s - The WordPress directory plugin. Discount Code: APIKEY25OFF', 'gmaps-api-key' ), '<a target="_blank" href="https://wpgeodirectory.com/" >', '</a>' ); ?></p>
+            </div>
+			<?php
+		}
+	}
+}
+
+add_action( 'admin_notices', 'rgmk_show_geodirectory_offer' );
+
+/**
+ * Add Google Maps API callback script to head.
+ *
+ * @since 1.2.4
+ */
+function rgmk_add_callback_script() {
+	$script = '<script type="text/javascript">function rgmkInitGoogleMaps(){window.rgmkGoogleMapsCallback=true;try{jQuery(document).trigger("rgmkGoogleMapsLoad")}catch(err){}}</script>';
+
+	/**
+	 * Filters the Google Maps JavaScript callback.
+	 *
+	 * @since 2.2.23
+	 *
+	 * @param string $script The callback script.
+	 */
+	$script = apply_filters( 'rgmk_google_map_callback_script', $script );
+
+	echo $script;
+}
+add_action( 'wp_head', 'rgmk_add_callback_script', 1 );
+add_action( 'admin_head', 'rgmk_add_callback_script', 1 );
---
+++ plugins/api-key-for-google-maps/readme.txt
@@ -0,0 +1,98 @@
+=== API KEY for Google Maps ===
+Contributors: stiofansisland, paoltaia
+Tags:  Google Maps, Google Maps KEY, Google Maps API KEY, Google Maps callback, Google Maps API callback
+Donate link: https://wpgeodirectory.com
+Requires at least: 5.0
+Tested up to: 6.4
+Stable tag: 1.2.10
+Requires PHP: 5.6
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Retroactively add Google Maps API KEY to any theme or plugin.
+
+== Description ==
+
+Retroactively add Google Maps API KEY to any theme or plugin.
+
+Simply activate, go to Settings>Google API KEY and enter your key.
+The plugin will then attempt to add this key to all the places it is needed on the front of your website.
+NOTE: this will only work if the Google API has been added as per WordPress standards)
+
+Since January 2023 Google Maps JavaScript API requires callback parameter. This plugin also fixes JavaScript Error: [Loading the Google Maps JavaScript API without a callback is not supported](https://developers.google.com/maps/documentation/javascript/url-params#required_parameters).
+
+The plugin was created by the GeoDirectory team: <https://wpgeodirectory.com>
+
+== Installation ==
+
+= Minimum Requirements =
+
+* WordPress 5.0 or greater
+* PHP version 5.6 or greater
+* MySQL version 5.0 or greater
+
+= Automatic installation =
+
+Automatic installation is the easiest option. To do an automatic install log in to your WordPress dashboard, navigate to the Plugins menu and click Add New.
+
+In the search field type Google Maps API KEY and click Search Plugins. Once you've found the plugin you install it by simply clicking Install Now.
+
+= Manual installation =
+
+The manual installation method involves downloading the plugin and uploading it to your webserver via your favourite FTP application. The WordPress codex will tell you more [here](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
+= Updating =
+
+Automatic updates should seamlessly work. We always suggest you backup up your website before performing any automated update to avoid unforeseen problems.
+
+== Frequently Asked Questions ==
+
+Ask and they shall be answered
+
+== Screenshots ==
+
+1. Settings page.
+2. Generate API KEY.
+3. Copy API KEY, paste in Settings and save.
+
+== Changelog ==
+
+= 1.2.10 (2023-12-06) =
+* WordPress v6.4 compatibility check - CHANGED
+
+= 1.2.9 (2023-08-10) =
+* WordPress v6.3 compatibility - CHANGED
+
+= 1.2.8 (2023-03-30) =
+* WordPress v6.2 compatibility - CHANGED
+
+= 1.2.7 (2023-02-02) =
+* Add .gitattributes file - ADDED
+* Generate Google API Key is no longer working - FIXED
+* Loading the Google Maps JavaScript API without a callback is not supported - CHANGED
+
+= 1.2.3 =
+* Plugin version update - CHANGED
+
+= 1.2.2 =
+* Compatibility checked with WordPress 6.0 - CHECKED
+* Now tries to add api key even if no key param is found - CHANGED
+* Now only users with "manage_options" ability can update the API key - SECURITY
+
+= 1.2.1 =
+* Compatibility checked with WordPress 5.9
+
+= 1.2.0 =
+
+iframe api generation broken (by Google iframe restrictions) changed to new window popup - FIXED
+Updated Generate API KEY button to add access for all APIs - CHANGED
+
+= 1.1.0 =
+
+Added a Generate API KEY button for easier generation of API KEY - ADDED
+
+= 1.0.0 =
+
+initial release
+
+== Upgrade Notice ==
