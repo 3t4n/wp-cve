84659a9ca59366e85358ddaa2e62ec9d57a3dfb8
root <root@vultr>
2024-03-14 08:38:30 +0000

    Added plugin easy-svg

---
+++ plugins/easy-svg/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'benjaminzekavica/easy-svg',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '207a98b8a35463f26ef0914f86c9b670b0b3a9f2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'benjaminzekavica/easy-svg' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '207a98b8a35463f26ef0914f86c9b670b0b3a9f2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'enshrined/svg-sanitize' => array(
+            'pretty_version' => '0.18.0',
+            'version' => '0.18.0.0',
+            'reference' => '6a2c069dab3843ca4d887ff09c972fc7033888d0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../enshrined/svg-sanitize',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
