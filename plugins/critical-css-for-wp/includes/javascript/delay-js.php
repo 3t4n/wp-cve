6ee49bafcda103375f5e1993429bd0da2890e920
root <root@vultr>
2024-03-14 02:00:15 +0000

    Added plugin critical-css-for-wp

---
+++ plugins/critical-css-for-wp/includes/javascript/delay-js.php
@@ -0,0 +1,325 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+function ccwp_get_atts_array( $atts_string ) {
+
+	if ( ! empty( $atts_string ) ) {
+		$atts_array = array_map(
+			function( array $attribute ) {
+				return $attribute['value'];
+			},
+			wp_kses_hair( $atts_string, wp_allowed_protocols() )
+		);
+		return $atts_array;
+	}
+	return false;
+}
+
+function ccwp_get_atts_string( $atts_array ) {
+
+	if ( ! empty( $atts_array ) ) {
+		$assigned_atts_array = array_map(
+			function( $name, $value ) {
+				if ( $value === '' ) {
+					return $name;
+				}
+				return sprintf( '%s="%s"', $name, esc_attr( $value ) );
+			},
+			array_keys( $atts_array ),
+			$atts_array
+		);
+		$atts_string         = implode( ' ', $assigned_atts_array );
+		return $atts_string;
+	}
+	return false;
+}
+
+function ccwp_delay_js_main() {
+
+	$is_admin = current_user_can( 'manage_options' );
+
+	if ( is_admin() || $is_admin ) {
+		return;
+	}
+
+	if ( function_exists( 'is_checkout' ) && is_checkout() || ( function_exists( 'is_feed' ) && is_feed() ) ) {
+		return;
+	}
+	if ( class_exists( 'next_article_layout' ) ) {
+		return;
+	}
+
+	if ( function_exists( 'elementor_load_plugin_textdomain' ) && isset(\Elementor\Plugin::$instance->preview) && \Elementor\Plugin::$instance->preview->is_preview_mode() ) {
+		return;
+	}
+
+	add_action( 'wp_footer', 'ccwp_delay_js_load', PHP_INT_MAX );
+
+	if ( ccwp_check_js_defer() ) {
+		add_filter( 'rocket_delay_js_exclusions', 'ccwp_add_rocket_delay_js_exclusions' );
+		return;
+	}
+}
+add_action( 'wp', 'ccwp_delay_js_main' );
+
+function ccwp_add_rocket_delay_js_exclusions( $patterns ) {
+	$patterns[] = 'ccwp-delayed-scripts';
+	return $patterns;
+}
+
+function ccwp_delay_js_html( $html ) {
+
+	$html_no_comments = $html;// preg_replace('/<!--(.*)-->/Uis', '', $html).
+	preg_match_all( '#(<script\s?([^>]+)?\/?>)(.*?)<\/script>#is', $html_no_comments, $matches );
+	if ( ! isset( $matches[0] ) ) {
+		return $html;
+	}
+	$combined_ex_js_arr = array();
+	foreach ( $matches[0] as $i => $tag ) {
+		$atts_array = ! empty( $matches[2][ $i ] ) ? ccwp_get_atts_array( $matches[2][ $i ] ) : array();
+		if ( isset( $atts_array['type'] ) && stripos( $atts_array['type'], 'javascript' ) == false ||
+			isset( $atts_array['id'] ) && stripos( $atts_array['id'], 'corewvps-mergejsfile' ) !== false ||
+			isset( $atts_array['id'] ) && stripos( $atts_array['id'], 'corewvps-cc' ) !== false
+		) {
+			continue;
+		}
+		$delay_flag       = false;
+		$excluded_scripts = array(
+			'ccwp-delayed-scripts',
+		);
+
+		if ( ! empty( $excluded_scripts ) ) {
+			foreach ( $excluded_scripts as $excluded_script ) {
+				if ( strpos( $tag, $excluded_script ) !== false ) {
+					continue 2;
+				}
+			}
+		}
+
+		$delay_flag = true;
+		if ( ! empty( $atts_array['type'] ) ) {
+			$atts_array['data-ccwp-type'] = $atts_array['type'];
+		}
+
+		$atts_array['type'] = 'ccwpdelayedscript';
+		$atts_array['defer'] = 'defer';
+		if ( isset( $atts_array['src'] ) && ! empty( $atts_array['src'] ) ) {
+			$regex = ccwp_delay_exclude_js();
+
+			if ( $regex && preg_match( '#(' . $regex . ')#', $atts_array['src'] ) ) {
+				$combined_ex_js_arr[] = $atts_array['src'];
+				$include = false;
+			}
+		}
+		if ( $include && isset( $atts_array['id'] ) ) {
+			$regex     = ccwp_delay_exclude_js();
+			$file_path = $atts_array['id'];
+			if ( $regex && preg_match( '#(' . $regex . ')#', $file_path ) ) {
+				$include = false;
+			}
+		}
+		if ( $include && isset( $matches[3][ $i ] ) ) {
+			$regex     = ccwp_delay_exclude_js();
+			$file_path = $matches[3][ $i ];
+			if ( $regex && preg_match( '#(' . $regex . ')#', $file_path ) ) {
+				$include = false;
+			}
+		}
+		if ( isset( $atts_array['src'] ) && ! $include ) {
+			$include = true;
+
+		}
+
+		if ( $delay_flag ) {
+
+			$delayed_atts_string = ccwp_get_atts_string( $atts_array );
+			$delayed_tag         = sprintf( '<script %1$s>', $delayed_atts_string ) . ( ! empty( $matches[3][ $i ] ) ? $matches[3][ $i ] : '' ) . '</script>';
+			$html                = str_replace( $tag, $delayed_tag, $html );
+			continue;
+		}
+	}
+	return $html;
+}
+
+function ccwp_delay_exclude_js() {
+	$settings             = critical_css_defaults();
+	$inputs['exclude_js'] = array();
+	$excluded_files       = array();
+	if ( $inputs['exclude_js'] ) {
+		foreach ( $inputs['exclude_js'] as $i => $excluded_file ) {
+			// Escape characters for future use in regex pattern.
+			$excluded_files[ $i ] = str_replace( '#', '\#', $excluded_file );
+		}
+	}
+	if ( is_array( $excluded_files ) ) {
+		return implode( '|', $excluded_files );
+	} else {
+		return '';
+	}
+}
+
+function ccwp_delay_js_load() {
+	$settings = critical_css_defaults();
+	if ( ( isset( $settings['ccfwp_defer_css'] ) && $settings['ccfwp_defer_css'] == 'off' ) ) {
+		return;
+	}
+
+	$ccfwp_defer_time = intval( $settings['ccfwp_defer_time'] );
+		$js_content   = '<script type="text/javascript" id="ccwp-delayed-scripts" data-two-no-delay="true">
+			let ccwpDOMLoaded=!1;
+			let ccwp_loaded = false;
+			let resources_length=0;
+			let resources =undefined;
+			let is_last_resource = 0;
+			ccwpUserInteractions=["keydown","mousemove","wheel","touchmove","touchstart","touchend","touchcancel","touchforcechange"];
+			
+				ccwpUserInteractions.forEach(function(e){
+					window.addEventListener(e,calculate_load_times);
+				});
+			
+           function calculate_load_times() {
+                // Check performance support
+                if (performance === undefined) {
+                    console.log("Performance NOT supported");
+                    return;
+                }
+                // Get a list of "resource" performance entries
+                resources = performance.getEntriesByType("resource");
+                if (resources === undefined || resources.length <= 0) {
+                    console.log("NO Resource performance records");
+                }
+                if(resources.length){
+                    resources_length=resources.length;
+                }
+                for(let i=0; i < resources.length; i++) {
+                    if(resources[i].responseEnd>0){
+                        is_last_resource = is_last_resource + 1;
+                    }
+                }
+                let uag = navigator.userAgent;
+                let gpat = /Google Page Speed Insights/gm;
+                let gres = uag.match(gpat);
+                let cpat = /Chrome-Lighthouse/gm;
+                let cres = uag.match(cpat);
+                let wait_till=' . esc_attr( $ccfwp_defer_time ) . ';
+                let new_ua = "Mozilla/5.0 (Linux; Android 11; moto g power (2022)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36";
+                let new_ua2 = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36";
+                if(gres || cres || uag==new_ua || uag==new_ua2){
+                    wait_till = 3000;
+                  }
+                if(is_last_resource==resources.length){
+                    setTimeout(function(){
+						console.log("ccwpTriggerDelayedScripts timeout : "+wait_till);
+                        ccwpTriggerDelayedScripts();
+                    },wait_till);
+                }
+            }
+            window.addEventListener("load", function(e) {
+				   console.log("load complete");
+				    setTimeout(function(){
+                        calculate_load_times();
+                    },100);
+            });
+
+            async function ccwpTriggerDelayedScripts() {
+                if(ccwp_loaded){ return ;}
+				 
+				 ccwpPreloadStyles();
+                 ccwpPreloadDelayedScripts();
+				 ccwpLoadCss();
+				 ccwpScriptLoading();
+                 ccwp_loaded=true;
+            }
+			 function ccwpPreloadStyles() {
+              let e = document.createDocumentFragment();
+              var cssEle = document.querySelectorAll("link[rel=ccwpdelayedstyle]");
+              for(let i=0; i <= cssEle.length;i++){
+                  if(cssEle[i]){
+                      cssEle[i].href = removeVersionFromLink(cssEle[i].href);
+                      let r = document.createElement("link");
+                      r.href = cssEle[i].href;
+                      r.rel = "preload";
+                      r.as = "style";
+                      e.appendChild(r);
+                  }
+                  }
+             document.head.appendChild(e);
+          }
+            function ccwpPreloadDelayedScripts() {
+                var e = document.createDocumentFragment();
+                document.querySelectorAll("script[type=ccwpdelayedscript]").forEach(function(t) {
+                    var n = removeVersionFromLink(t.getAttribute("src"));
+                    if (n) {
+                        t.setAttribute("src", n);
+                        var r = document.createElement("link");
+                        r.href = n, r.rel = "preload", r.as = "script", e.appendChild(r)
+                    }
+                }), document.head.appendChild(e)
+            }
+			
+			function ccwpScriptLoading(){
+				 var jsEle = document.querySelectorAll("script[type=ccwpdelayedscript]");
+				  jsEle.forEach(function(t) {
+							t.type = "text/javascript";
+							if(t.src)
+							{
+							  t.src = removeVersionFromLink(t.src);
+							}
+                });
+            }
+
+             function ccwpLoadCss(){
+				 
+              var cssEle = document.querySelectorAll("link[rel=ccwpdelayedstyle]");
+                for(let i=0; i <= cssEle.length;i++){
+                    if(cssEle[i]){
+                        cssEle[i].href = removeVersionFromLink(cssEle[i].href);
+                        cssEle[i].rel = "stylesheet";
+                        cssEle[i].type = "text/css";
+                    }
+                }
+
+                var cssEle = document.querySelectorAll("style[type=ccwpdelayedstyle]");
+                for(let i=0; i <= cssEle.length;i++){
+                    if(cssEle[i]){
+                        cssEle[i].type = "text/css";
+                    }
+                }
+            }
+            function removeVersionFromLink(link)
+            {
+                if(ccwpIsValidUrl(link))
+                {
+                    const url = new URL(ccwpFormatLink(link));
+                    url.searchParams.delete("ver");
+                    url.searchParams.delete("time");
+                    return url.href;
+                }
+                else{
+                    return link;
+                }
+            }
+            function ccwpIsValidUrl(urlString)
+            {
+                if(urlString){
+                    var expression =/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+                    var regex = new RegExp(expression);
+                    return urlString.match(regex);
+                }
+                return false;
+            }
+            function ccwpFormatLink(link)
+            {
+                let http_check=link.match("http:");
+                let https_check=link.match("https:");
+                if(!http_check && !https_check)
+                {
+                    return location.protocol+link;
+                }
+                return link;
+            }
+			</script>';
+
+	echo $js_content;
+}
---
+++ plugins/critical-css-for-wp/readme.txt
@@ -0,0 +1,109 @@
+=== Reduce Unused CSS Solution with Critical CSS For WP ===
+Contributors: magazine3
+Tags: Critical, Critical CSS, Core Web Vital, Cache, SEO
+Requires at least: 3.0
+Tested up to: 6.4
+Stable tag: 1.0.12
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Description ==
+
+Critical CSS For WP removes the unused CSS which helps to paint fast and render the above fold content, before downloading the complete css files. We intends to provide great experience to the web page visitors by improving the performance of the web page.
+
+
+### Works well with:
+
+* WP Rocket
+* Autoptimize
+* Aruba HiSpeed Cache
+* Breeze – WordPress Cache
+* Clearfy Cache
+* Cache Enabler
+* GoDaddy Cache
+* Hummingbird
+* Kinsta Cache
+* LiteSpeed Cache
+* Nginx Helper
+* Redis Object Cache
+* SiteGround SuperCacher
+* Speed Booster Pack ⚡ PageSpeed Optimization Suite
+* WP Super Cache
+* WP Fastest Cache
+* W3 Total Cache
+* WP-Optimize
+* WPEngine Cache
+* WP Speed of Light
+* 10Web Booster – Website speed optimization, Cache & Page Speed optimizer
+
+
+**We Act Fast on Feedback!**
+We are actively developing this plugin and our aim is to make this plugin into the #1 solution for generating Critical CSS in the world. You can [Request a Feature or Report a Bug](https://github.com/ahmedkaludi/critical-css-for-wp/issues).
+
+**Technical Support**
+Support is provided in [Forum](https://wordpress.org/support/plugin/critical-css-for-wp/).
+
+**Would you like to contribute?**
+You may now contribute to this Critical CSS For WP plugin on Github: [View repository](https://github.com/ahmedkaludi/critical-css-for-wp) on Github
+
+
+== Changelog ==
+
+= 1.0.12 (09 January 2024) = 
+* Tested : Test with wordpress 6.4 and update readme.txt #41
+* Fixed : Category URLs are also getting cached even if the checkbox is not selected in the settings #40
+
+= 1.0.11 (07 November 2023) = 
+* Fixed : Critical Error while activating #38
+
+= 1.0.10 (13 September 2023) = 
+* Fixed : Post / Pages not adding to queue for generating #35
+
+= 1.0.9 (21 August 2023) = 
+* Added : Create support links & other links for users to reach #11
+* Compatibility : Updated code according to WP Standard #32
+* Compatibility : Added compatibility with 10web Booster #31
+* Compatibility : Tested with WordPress 6.3 and update readme.txt #29
+* Fixed : Improvement in Critical CSS generation. #14
+* Fixed : Confirmation popup should display after sending message in support form of plugin #23
+* Fixed : Issue with send message button in Support section #28
+* Fixed : Warning showing in error log #30
+* Improved : add github links and other stuff in the readme.txt #4
+
+= 1.0.8 (26 May 2023) = 
+* Fixed : Critical css not loading on frontend using FlexMLS_IDX plugin #18
+* Fixed : Critical CSS not generated in subdomain due to SSL issue #22
+* Fixed : Reduce Used CSS showing on PageSpeed
+* Improved : Fixed  assets links with double slash(//)
+
+
+= 1.0.7 (04 March 2023) = 
+* Fixed : Critical css is not generated while using file_get_contents #17
+* Fixed : Critical css not loading on frontend using FlexMLS_IDX plugin #18
+* Fixed : Delayed CSS loading only on scroll/click event #19
+* Fixed : Option to load critical CSS without Delay #20
+
+= 1.0.6 (10 February 2023) = 
+* jQuery is not defined and related js issues #15
+
+= 1.0.5 (02 February 2023) = 
+* Make plugin compatible with WP rocket and Elementor #12
+* Images and Google Ads not loading #8
+
+= 1.0.4 (17 January 2023) = 
+* Make plugin WP multisite compatible #9
+
+= 1.0.3 (10 January 2023) = 
+* Added compatibility for WP Rocket.
+* Improved Critical CSS generation.
+
+= 1.0.2 (19 December 2022) = 
+* Added the code to improve the performance of the web page.
+
+= 1.0.1 (15 November 2022) = 
+* Resolved the issue of Fatal error: Composer detected issues in your platform.
+
+= 1.0.0 (06 September 2022) =
+
+* Initial Releases.
