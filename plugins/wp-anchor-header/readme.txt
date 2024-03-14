d9e30f39975c3e82c97c5e044b36fe2aaf622722
root <root@vultr>
2024-03-14 16:27:02 +0000

    Added plugin wp-anchor-header

---
+++ plugins/wp-anchor-header/readme.txt
@@ -0,0 +1,73 @@
+=== WP Anchor Header ===
+Contributors: PerS
+Donate link: http://soderlind.no/donate/
+Tags: header, link
+Requires at least: 4.0
+Tested up to: 4.9
+Stable tag: 0.2.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+WP Anchor Header generates anchored headings.
+
+== Description ==
+
+WP Anchor Header generates anchored headings (H1-H6), creating a heading like this:
+
+`
+<h2 id="the-heading">
+   The Heading<a class="anchorlink dashicons-before" href="#the-heading"></a>
+</h2>
+`
+
+The links are styled using the following CSS:
+
+`
+/* show IDs for anchors */
+h1[id]:hover a.anchorlink:before,
+h2[id]:hover a.anchorlink:before,
+h3[id]:hover a.anchorlink:before,
+h4[id]:hover a.anchorlink:before,
+h5[id]:hover a.anchorlink:before,
+h6[id]:hover a.anchorlink:before {
+    content: "\f103"; /*dashicons-admin-links*/
+    color: #888;
+    font-size: smaller;
+    text-decoration: none;
+    vertical-align: baseline;
+}
+a.anchorlink {
+    text-decoration: none;
+    margin-left: 0.5em;
+    font-size: smaller;
+}
+`
+
+=== Credits ===
+
+The plugin is inspired by Christian Weiske and his article [Usability: Clickable heading links](http://cweiske.de/tagebuch/html-heading-links.htm).
+
+== Installation ==
+
+1. Download the plugin and extract the read-online.zip
+1. Upload the extracted `read-online` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+
+== Changelog ==
+= 0.2.3 =
+* Tested & found compatible with WP 4.7.
+= 0.2.2 =
+* Tested & found compatible with WP 4.6.
+= 0.2.1 =
+* Fix: Don't run if `the_content` is empty
+= 0.2.0 =
+* Update plugin for [WordPress Coding Standards](https://make.wordpress.org/core/handbook/best-practices/coding-standards/).
+* Tested & found compatible with WP 4.5.
+= 0.1.8 =
+* Fixed UTF-8 parsing bug, added `mb_convert_encoding` to loadHTML(): `@$doc->loadHTML( mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8') );`
+* Added singleton
+* Added comments to code
+= 0.1.5 =
+* Initial release
---
+++ plugins/wp-anchor-header/wp-anchor-header.php
@@ -0,0 +1,110 @@
+<?php
+/*
+Plugin Name: WP Anchor Header
+Plugin URI: https://github.com/soderlind/wp-anchor-header
+Description: Generates anchored headings.
+Author: Per Soderlind
+Version: 0.2.3
+Author URI: http://soderlind.no
+*/
+
+if ( defined( 'ABSPATH' ) ) {
+	Anchor_Header::instance();
+}
+
+define( 'ANCHORHEADER_URL',   plugin_dir_url( __FILE__ ) );
+define( 'ANCHORHEADER_VERSION', '0.2.3' );
+
+
+class Anchor_Header {
+
+	/**
+	 * Refers to a single instance of this class
+	 *
+	 * @var null
+	 */
+	private static $instance = null;
+
+	/**
+	 * Creates or returns an instance of this class.
+	 *
+	 * @since 0.1.8
+	 * @return object Anchor_Header, a single instance of this class.
+	 */
+	public static function instance() {
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Load style and attach $this->the_content to the the_content filter
+	 *
+	 * @since 0.1.0
+	 */
+	function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'add_script_style' ) );
+		add_filter( 'the_content', array( $this, 'the_content' ) );
+	}
+
+	/**
+	 * Using DOMDocument, parse the content and add anchors to headers (H1-H6)
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string  $content The content
+	 * @return string          the content, updated if the content has H1-H6
+	 */
+	function the_content( $content ) {
+
+		if ( ! is_singular() || '' == $content ) {
+			return $content;
+		}
+		$anchors = array();
+		$doc = new DOMDocument();
+		// START LibXML error management.
+		// Modify state
+		$libxml_previous_state = libxml_use_internal_errors( true );
+		$doc->loadHTML( mb_convert_encoding( $content, 'HTML-ENTITIES', 'UTF-8' ) );
+		// handle errors
+		libxml_clear_errors();
+		// restore
+		libxml_use_internal_errors( $libxml_previous_state );
+		// END LibXML error management.
+
+		foreach ( array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ) as $h ) {
+			$headings = $doc->getElementsByTagName( $h );
+			foreach ( $headings as $heading ) {
+				$a = $doc->createElement( 'a' );
+				$newnode = $heading->appendChild( $a );
+				$newnode->setAttribute( 'class', 'anchorlink dashicons-before' );
+				// @codingStandardsIgnoreStart
+				// $heading->nodeValue is from an external libray. Ignore the standard check sinice it doesn't fit the WordPress-Core standard
+				$slug = $tmpslug = sanitize_title( $heading->nodeValue );
+				// @codingStandardsIgnoreEnd
+				$i = 2;
+				while ( false !== in_array( $slug, $anchors ) ) {
+					$slug = sprintf( '%s-%d', $tmpslug, $i++ );
+				}
+				$anchors[] = $slug;
+				$heading->setAttribute( 'id', $slug );
+				$newnode->setAttribute( 'href', '#' . $slug );
+			}
+		}
+		return $doc->saveHTML();
+	}
+
+	/**
+	 * Enable dashicons on the front-end
+	 * Load style
+	 *
+	 * @since 0.1.0
+	 */
+	function add_script_style() {
+		if ( is_singular() ) {
+			wp_enqueue_style( 'dashicons' );
+			wp_enqueue_style( 'achored-header',  ANCHORHEADER_URL . 'css/achored-header.css', array( 'dashicons' ), ANCHORHEADER_VERSION );
+		}
+	}
+} // class Anchor_Header
