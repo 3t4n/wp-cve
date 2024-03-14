171b0f3290444511372f8ffcbf5dc6feebc39bcb
root <root@vultr>
2024-03-14 13:24:07 +0000

    Added plugin advanced-accordion-block

---
+++ plugins/advanced-accordion-block/build/accordion-item/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'bbd3e8cf45be059c383b');
---
+++ plugins/advanced-accordion-block/build/group-accordion/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aa13b3806e205bc1f9a9');
