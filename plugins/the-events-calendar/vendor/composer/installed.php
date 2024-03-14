85c7a4c89cb13610e73e29e5dfced3825544d573
root <root@vultr>
2024-03-14 08:44:51 +0000

    Added plugin the-events-calendar

---
+++ plugins/the-events-calendar/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/the-events-calendar',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '96c9239f979f2b165fb2343ceab949f4787603d8',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'the-events-calendar/the-events-calendar' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '96c9239f979f2b165fb2343ceab949f4787603d8',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'woocommerce/action-scheduler' => array(
+            'pretty_version' => '3.7.1',
+            'version' => '3.7.1.0',
+            'reference' => 'bdaecb016e0358065a8bf76a41fdd60772cacaa5',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../woocommerce/action-scheduler',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
