79a35359ed2f795b78fca3250cc66fadd8d06a11
root <root@vultr>
2024-03-14 03:05:54 +0000

    Added plugin wp-rollback

---
+++ plugins/wp-rollback/build/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-components', 'wp-dom-ready', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-url'), 'version' => 'c0e2bfebedaee8fe030d');
---
+++ plugins/wp-rollback/build/themes.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '35ad3c9364cec1392232');
