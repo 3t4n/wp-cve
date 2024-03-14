82185319e1262c2bf6dc8eeedce3a0c45092e9b6
root <root@vultr>
2024-03-14 22:55:34 +0000

    Added plugin display-a-meta-field-as-block

---
+++ plugins/display-a-meta-field-as-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '318b7ad34c4ac36e5a1b');
---
+++ plugins/display-a-meta-field-as-block/build/settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0304a74bab1e3dfb133c');
