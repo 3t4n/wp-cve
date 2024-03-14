905f2e2ec443fcd4cee807f12132e22ea13017fc
root <root@vultr>
2024-03-14 21:12:28 +0000

    Added plugin cf7-to-zapier

---
+++ plugins/cf7-to-zapier/includes/functions-debug.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * A helper to debug
+ *
+ * @package         Cf7_To_Zapier
+ * @since           2.3.0
+ */
+
+defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
+
+function cftz_activated_debug_functions() {
+    return ( defined( 'WP_DEBUG' ) && WP_DEBUG ) && ! ( defined( 'CFTZ_REMOVE_DEBUG_FUNCTIONS' ) && CFTZ_REMOVE_DEBUG_FUNCTIONS );
+}
+
+/**
+ * Emulate dd function from laravel
+ *
+ * @SuppressWarnings(PHPMD)
+ */
+if ( ! function_exists( 'dd' ) && cftz_activated_debug_functions() ) {
+    function dd( $param, $include_pre = true ) {
+        echo $include_pre ? '<pre>' : '';
+        print_r( $param );
+        echo $include_pre ? '</pre>' : '';
+        exit;
+    }
+}
+
+/**
+ * Emulate dump function from
+ * laravel but write to logs
+ */
+if ( ! function_exists( 'dump' ) && cftz_activated_debug_functions() ) {
+    function dump( $param ) {
+        error_log( $param );
+    }
+}
+
