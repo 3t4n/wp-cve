bf02094b9cdfeb2041869cb810ded14742d1d51b
root <root@vultr>
2024-03-14 14:54:48 +0000

    Added plugin action-scheduler

---
+++ plugins/action-scheduler/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'woocommerce/action-scheduler',
+        'pretty_version' => 'dev-release/3.7.2',
+        'version' => 'dev-release/3.7.2',
+        'reference' => 'cb3c7854b31a285b46964931425fef6f485be145',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'woocommerce/action-scheduler' => array(
+            'pretty_version' => 'dev-release/3.7.2',
+            'version' => 'dev-release/3.7.2',
+            'reference' => 'cb3c7854b31a285b46964931425fef6f485be145',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
