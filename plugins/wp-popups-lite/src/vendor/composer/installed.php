bfcd0b3a3d4999f8f56ef04aae5d3bb0163acdf9
root <root@vultr>
2024-03-14 12:53:59 +0000

    Added plugin wp-popups-lite

---
+++ plugins/wp-popups-lite/src/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2669f863c63390284b920df82f9819f5cdf09d55',
+    'name' => 'timersys/wppopups',
+  ),
+  'versions' => 
+  array (
+    'jaybizzle/crawler-detect' => 
+    array (
+      'pretty_version' => 'v1.2.116',
+      'version' => '1.2.116.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '97e9fe30219e60092e107651abb379a38b342921',
+    ),
+    'mobiledetect/mobiledetectlib' => 
+    array (
+      'pretty_version' => '2.8.41',
+      'version' => '2.8.41.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc9cccd4d3706d5a7537b562b59cc18f9e4c0cb1',
+    ),
+    'timersys/wppopups' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2669f863c63390284b920df82f9819f5cdf09d55',
+    ),
+  ),
+);
