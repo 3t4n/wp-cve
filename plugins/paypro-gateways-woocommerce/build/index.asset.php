28f76667cf2b3536c790b18e1c8c6560f45774fd
root <root@vultr>
2024-03-14 07:34:42 +0000

    Added plugin paypro-gateways-woocommerce

---
+++ plugins/paypro-gateways-woocommerce/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => 'fbb6cab4b737a1da878f');
