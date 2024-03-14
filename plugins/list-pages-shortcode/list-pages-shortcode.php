c3986adff51180f410bc411ab02b7d1d06c82315
root <root@vultr>
2024-03-14 13:58:43 +0000

    Added plugin list-pages-shortcode

---
+++ plugins/list-pages-shortcode/list-pages-shortcode.php
@@ -0,0 +1,386 @@
+<?php
+
+/**
+ * Plugin Name: List Pages Shortcode
+ * Plugin URI: http://wordpress.org/extend/plugins/list-pages-shortcode/
+ * Description: Introduces the [list-pages], [sibling-pages] and [child-pages] <a href="http://codex.wordpress.org/Shortcode_API">shortcodes</a> for easily displaying a list of pages within a post or page.  Both shortcodes accept all parameters that you can pass to the <a href="http://codex.wordpress.org/Template_Tags/wp_list_pages">wp_list_pages()</a> function.  For example, to show a page's child pages sorted by title simply add [child-pages sort_column="post_title"] in the page's content.
+ * Author: Ben Huson, Aaron Harp
+ * Version: 1.7.7
+ * Author URI: http://www.aaronharp.com
+ */
+
+add_shortcode( 'child-pages', array( 'List_Pages_Shortcode', 'shortcode_list_pages' ) );
+add_shortcode( 'sibling-pages', array( 'List_Pages_Shortcode', 'shortcode_list_pages' ) );
+add_shortcode( 'list-pages', array( 'List_Pages_Shortcode', 'shortcode_list_pages' ) );
+add_filter( 'list_pages_shortcode_excerpt', array( 'List_Pages_Shortcode', 'excerpt_filter' ) );
+
+class List_Pages_Shortcode {
+
+	public function __construct() {
+		// @todo  Deprecate use of constructor
+	}
+
+	public function List_Pages_Shortcode() {
+		// @todo  Deprecate use of PHP4 constructor
+	}
+
+	public static function shortcode_list_pages( $atts, $content, $tag ) {
+
+		global $post;
+
+		$atts = self::parse_shortcode_atts( $atts, $tag );
+		$atts = apply_filters( 'shortcode_list_pages_attributes', $atts, $content, $tag );
+
+		do_action( 'shortcode_list_pages_before', $atts, $content, $tag );
+
+		// Catch <ul> tags in wp_list_pages().
+		if ( 'ul' !== $atts['list_type'] ) {
+			add_filter( 'wp_list_pages', array( 'List_Pages_Shortcode', 'ul2list_type' ), 10, 2 );
+		}
+
+		// Create output.
+		$atts = self::prepare_list_pages_atts( $atts, $tag );
+		$out  = wp_list_pages( $atts );
+		remove_filter( 'wp_list_pages', array( 'List_Pages_Shortcode', 'ul2list_type' ), 10 );
+		if ( ! empty( $out ) && ! empty( $atts['list_type'] ) ) {
+			$out = '<' . sanitize_key( $atts['list_type'] ) . ' class="' . esc_attr( $atts['class'] ) . '">' . $out . '</' . sanitize_key( $atts['list_type'] ) . '>';
+		}
+		$out = apply_filters( 'shortcode_list_pages', $out, $atts, $content, $tag );
+
+		do_action( 'shortcode_list_pages_after', $atts, $content, $tag );
+
+		return $out;
+
+	}
+
+	protected static function parse_shortcode_atts( $atts, $tag ) {
+
+		// Allowed shortcode attributes.
+		$defaults = array(
+			'class'                => 'list-pages-shortcode ' . $tag,
+			'depth'                => 0,
+			'show_date'            => '',
+			'date_format'          => get_option( 'date_format' ),
+			'exclude'              => '',
+			'include'              => '',
+			'child_of'             => self::get_default_child_of( $tag ),
+			'list_type'            => 'ul',
+			'title_li'             => '',
+			'authors'              => '',
+			'sort_column'          => 'menu_order, post_title',
+			'sort_order'           => '',
+			'link_before'          => '',
+			'link_after'           => '',
+			'exclude_tree'         => '',
+			'meta_key'             => '',
+			'meta_value'           => '',
+			'post_type'            => 'page',
+			'offset'               => '',
+			'exclude_current_page' => 0,
+			'excerpt'              => 0,
+		);
+
+		$atts = shortcode_atts( $defaults, $atts );
+
+		// Validate/sanitize attributes.
+		$atts['class']                = self::sanitize_html_classes( $atts['class'] );
+		$atts['depth']                = absint( $atts['depth'] );
+		$atts['show_date']            = sanitize_text_field( $atts['show_date'] );
+		$atts['date_format']          = sanitize_text_field( $atts['date_format'] );
+		$atts['exclude']              = '' !== $atts['exclude'] ? self::sanitize_absints_string( $atts['exclude'] ) : '';
+		$atts['include']              = '' !== $atts['include'] ? self::sanitize_absints_string( $atts['include'] ) : '';
+		$atts['child_of']             = absint( $atts['child_of'] );
+		$atts['list_type']            = self::validate_list_type( $atts['list_type'] );
+		$atts['title_li']             = '' !== $atts['title_li'] ? wp_kses_post( $atts['title_li'] ) : '';
+		$atts['authors']              = '' !== $atts['authors'] ? self::sanitize_absints_string( $atts['authors'] ) : '';
+		$atts['sort_column']          = self::validate_sort_column( $atts['sort_column'] );
+		$atts['sort_order']           = 'DESC' === strtoupper( $atts['sort_order'] ) ? 'DESC' : 'ASC';
+		$atts['link_before']          = '' !== $atts['link_before'] ? wp_kses_post( $atts['link_before'] ) : '';
+		$atts['link_after']           = '' !== $atts['link_after'] ? wp_kses_post( $atts['link_after'] ) : '';
+		$atts['exclude_tree']         = '' !== $atts['exclude_tree'] ? self::sanitize_absints_string( $atts['exclude_tree'] ) : '';
+		$atts['meta_key']             = sanitize_key( $atts['meta_key'] );
+		$atts['meta_value']           = sanitize_text_field( $atts['meta_value'] );
+		$atts['post_type']            = self::validate_post_type( $atts['post_type'] );
+		$atts['offset']               = absint( $atts['offset'] );
+		$atts['exclude_current_page'] = absint( $atts['exclude_current_page'] );
+		$atts['excerpt']              = absint( $atts['excerpt'] );
+
+		// Extra attributes.
+		$atts['walker'] = new List_Pages_Shortcode_Walker_Page();
+
+		return $atts;
+
+	}
+
+	protected static function prepare_list_pages_atts( $atts, $tag ) {
+
+		global $post;
+
+		// Don't echo list pages output.
+		$atts['echo'] = 0;
+
+		// Exclude current page.
+		if ( $atts['exclude_current_page'] && absint( $post->ID ) ) {
+			if ( ! empty( $atts['exclude'] ) ) {
+				$atts['exclude'] .= ',';
+			}
+			$atts['exclude'] .= absint( $post->ID );
+		}
+
+		return $atts;
+
+	}
+
+	protected static function sanitize_html_classes( $class ) {
+
+		$class = array_map( 'sanitize_html_class', explode( ' ', $class ) );
+
+		return implode( ' ', $class );
+
+	}
+
+	protected static function sanitize_absints_string( $value ) {
+
+		if ( '' !== $value ) {
+			$values = explode( ',', $value );
+			$values = array_map( 'absint', array_map( 'trim', $values ) );
+			return implode( ',', array_filter( $values ) );
+		}
+
+		return $value;
+
+	}
+
+	protected static function validate_sort_column( $sort_column ) {
+
+		$valid_columns = array(
+			'post_author',
+			'post_date',
+			'post_title',
+			'post_name',
+			'post_modified',
+			'post_modified_gmt',
+			'menu_order',
+			'post_parent',
+			'ID',
+			'rand',
+			'comment_count',
+			'post_title',
+		);
+
+		$sort_columns = explode( ',', $sort_column );
+		$sort_columns = array_map( 'trim', $sort_columns );
+
+		$validated_columns = array_intersect( $sort_columns, $valid_columns );
+
+		if ( ! empty( $validated_columns ) ) {
+			return implode( ',', $validated_columns );
+		}
+
+		return 'post_title';
+
+	}
+
+	protected static function validate_post_type( $post_type ) {
+
+		$post_type = sanitize_key( $post_type );
+
+		if ( post_type_exists( $post_type ) && is_post_type_viewable( $post_type ) ) {
+			return $post_type;
+		}
+
+		return '';
+
+	}
+
+	protected static function get_default_child_of( $tag ) {
+
+		$post = get_post();
+
+		if ( 'child-pages' === $tag ) {
+			return $post->ID;
+		} elseif ( 'sibling-pages' === $tag ) {
+			return $post->post_parent;
+		}
+
+		return 0;
+
+	}
+
+	/**
+	 * UL 2 List Type
+	 * Replaces all <ul> tags with <{list_type}> tags.
+	 *
+	 * @param  string $output Output of wp_list_pages().
+	 * @param  array  $args shortcode_list_pages() args.
+	 * @return string HTML output.
+	 */
+	public static function ul2list_type( $output, $args = null ) {
+
+		$list_type = self::validate_list_type( $args['list_type'] );
+
+		if ( 'ul' !== $list_type ) {
+
+			// <ul>
+			$output = str_replace( '<ul>', '<' . $list_type . '>', $output );
+			$output = str_replace( '<ul ', '<' . $list_type . ' ', $output );
+			$output = str_replace( '</ul> ', '</' . $list_type . '>', $output );
+
+			// <li>
+			if ( 'ol' !== $list_type ) {
+				$list_type = 'span' === $list_type ? 'span' : 'div';
+				$output    = str_replace( '<li>', '<' . $list_type . '>', $output );
+				$output    = str_replace( '<li ', '<' . $list_type . ' ', $output );
+				$output    = str_replace( '</li> ', '</' . $list_type . '>', $output );
+			}
+
+		}
+
+		return $output;
+
+	}
+
+	/**
+	 * Excerpt Filter
+	 * Add a <div> around the excerpt by default.
+	 *
+	 * @param string $text Excerpt.
+	 * @return string Filtered excerpt.
+	 */
+	public static function excerpt_filter( $text ) {
+		if ( ! empty( $text ) ) {
+			return ' <div class="excerpt">' . wp_kses( $text, 'post' ) . '</div>';
+		}
+		return $text;
+	}
+
+	/**
+	 * Validate List Type
+	 *
+	 * @param  string $list_type  List type tag.
+	 * @return string              Valid tag.
+	 */
+	public static function validate_list_type( $list_type ) {
+
+		if ( ! empty( $list_type ) && in_array( $list_type, array( 'ul', 'div', 'span', 'article', 'aside', 'section' ), true ) ) {
+			return $list_type;
+		}
+
+		return 'ul';
+
+	}
+
+}
+
+/**
+ * Create HTML list of pages.
+ * A copy of the WordPress Walker_Page class which adds an excerpt.
+ */
+class List_Pages_Shortcode_Walker_Page extends Walker_Page {
+
+	/**
+	 * @see Walker::start_lvl()
+	 * @since 2.1.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int    $depth Depth of page. Used for padding.
+	 */
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent    = str_repeat( "\t", $depth );
+		$list_type = List_Pages_Shortcode::validate_list_type( $args['list_type'] );
+		$output   .= "\n$indent<" . $list_type . " class='children'>\n";
+	}
+
+	/**
+	 * @see Walker::end_lvl()
+	 * @since 2.1.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int    $depth Depth of page. Used for padding.
+	 */
+	public function end_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent    = str_repeat( "\t", $depth );
+		$list_type = List_Pages_Shortcode::validate_list_type( $args['list_type'] );
+		$output   .= "$indent</" . $list_type . ">\n";
+	}
+
+	/**
+	 * @see Walker::start_el()
+	 * @since 2.1.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param object $page Page data object.
+	 * @param int    $depth Depth of page. Used for padding.
+	 * @param array  $args
+	 * @param int    $current_page Page ID.
+	 */
+	public function start_el( &$output, $page, $depth = 0, $args = array(), $current_page = 0 ) {
+		if ( $depth ) {
+			$indent = str_repeat( "\t", $depth );
+		} else {
+			$indent = '';
+		}
+
+		$css_class = array( 'page_item', 'page-item-' . $page->ID );
+
+		if ( isset( $args['pages_with_children'][ $page->ID ] ) ) {
+			$css_class[] = 'page_item_has_children';
+		}
+
+		if ( ! empty( $current_page ) ) {
+			$_current_page = get_page( $current_page );
+			if ( in_array( $page->ID, $_current_page->ancestors, true ) ) {
+				$css_class[] = 'current_page_ancestor';
+			}
+			if ( $page->ID === $current_page ) {
+				$css_class[] = 'current_page_item';
+			} elseif ( $_current_page && $page->ID === $_current_page->post_parent ) {
+				$css_class[] = 'current_page_parent';
+			}
+		} elseif ( absint( get_option( 'page_for_posts' ) ) === $page->ID ) {
+			$css_class[] = 'current_page_parent';
+		}
+
+		$css_class = implode( ' ', apply_filters( 'page_css_class', $css_class, $page, $depth, $args, $current_page ) );
+
+		if ( '' === $page->post_title ) {
+			$page->post_title = sprintf( __( '#%d (no title)' ), $page->ID );
+		}
+
+		$args['link_before'] = empty( $args['link_before'] ) ? '' : wp_kses_post( $args['link_before'] );
+		$args['link_after']  = empty( $args['link_after'] ) ? '' : wp_kses_post( $args['link_after'] );
+
+		$item = '<a href="' . get_permalink( $page->ID ) . '">' . $args['link_before'] . apply_filters( 'the_title', $page->post_title, $page->ID ) . $args['link_after'] . '</a>';
+
+		if ( ! empty( $args['show_date'] ) ) {
+			if ( 'modified' === $args['show_date'] ) {
+				$time = $page->post_modified;
+			} else {
+				$time = $page->post_date;
+			}
+
+			$date_format = empty( $args['date_format'] ) ? '' : $args['date_format'];
+			$item       .= ' ' . mysql2date( $date_format, $time );
+		}
+
+		// Excerpt.
+		if ( $args['excerpt'] ) {
+			$item .= apply_filters( 'list_pages_shortcode_excerpt', $page->post_excerpt, $page, $depth, $args, $current_page );
+		}
+
+		$output .= $indent . '<li class="' . esc_attr( $css_class ) . '">' . apply_filters( 'list_pages_shortcode_item', $item, $page, $depth, $args, $current_page );
+	}
+
+}
+
+/**
+ * [shortcode_list_pages] Function
+ * Kept for legacy reasons in case people are using it directly.
+ */
+function shortcode_list_pages( $atts, $content, $tag ) {
+	return List_Pages_Shortcode::shortcode_list_pages( $atts, $content, $tag );
+}
+
+// @todo  Deprecate instance
+global $List_Pages_Shortcode;
+$List_Pages_Shortcode = new List_Pages_Shortcode();
---
+++ plugins/list-pages-shortcode/readme.txt
@@ -0,0 +1,188 @@
+=== List Pages Shortcode ===
+Contributors: husobj, aaron_guitar
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=aaron%40freshwebs%2enet&item_name=Fotobook%20Donation&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8
+Tags: shortcodes, pages, list pages, sibling pages, child pages, subpages
+Requires at least: 4.4.0
+Tested up to: 6.1.1
+Stable tag: 1.7.7
+License: GPLv2 or later
+
+Introduces the [list-pages], [sibling-pages] and [child-pages] shortcodes for easily displaying a list of pages within a post or page.
+
+== Description ==
+
+Introduces the [list-pages], [sibling-pages] and [child-pages] [shortcodes](http://codex.wordpress.org/Shortcode_API) for easily displaying a list of pages within a post or page.  Both shortcodes accept all parameters that you can pass to the [`wp_list_pages()`](http://codex.wordpress.org/Template_Tags/wp_list_pages) function with the addition of a class parameter.
+
+= Example Usage =
+
+*List pages sorted by title*
+
+`[list-pages sort_column="post_title"]`
+
+*List pages but exclude certain IDs and set the class of the list to "my-page-list"*
+
+`[list-pages exclude="17,38" class="my-page-list"]`
+
+*Show excerpt (for pages excerpt support will need adding manually or via the [Page Excerpt](https://wordpress.org/plugins/page-excerpt/) plugin)*
+
+`[list-pages excerpt="1"]`
+
+*List the current page's children, but only show the top level*
+
+`[child-pages depth="1"]`
+
+*List the current page's siblings and their subpages*
+
+`[sibling-pages depth="2"]`
+
+= Default Arguments =
+
+The default values are the same as for the [wp_list_pages()](http://codex.wordpress.org/Template_Tags/wp_list_pages) function except for title_li which defaults to nothing.  If a class is not specified, a default class of either "list-pages", "sibling-pages" or "child-pages" is given to the UL tag.  In addition, the echo parameter has no effect.
+
+In addition to the [wp_list_pages()](http://codex.wordpress.org/Template_Tags/wp_list_pages) arguments, you can also specify:
+
+* **list_type** *(string)* List tag. Defaults to `<ul>`.
+* **exclude_current_page** *(int)* Exclude the current page. Defaults to `0`.
+* **excerpt** *(int)* Show the page excerpt. Defaults to `0`.
+
+== Installation ==
+
+1. Download and unzip the most recent version of this plugin
+2. Upload the list-pages-shortcode folder to /path-to-wordpress/wp-content/plugins/
+3. Login to your WP Admin panel, click Plugins, and activate "List Pages Shortcode"
+
+== Frequently Asked Questions ==
+
+= How do I include a page excerpt? =
+
+Firstly you will need to add support for excerpt for your pages. You can either you this by using the [add_post_type_support()](http://codex.wordpress.org/Function_Reference/add_post_type_support) function or using a plugin like [Page Excerpt](http://wordpress.org/extend/plugins/page-excerpt/).
+
+You can also use the 'list_pages_shortcode_excerpt' filter to return or customize the excerpt for specific pages. The following example:
+`<?php
+function my_list_pages_shortcode_excerpt( $excerpt, $page, $depth, $args ) {
+	return $excerpt . '...';
+}
+add_filter( 'list_pages_shortcode_excerpt', 'my_list_pages_shortcode_excerpt', 10, 4 );
+?>`
+
+You can then include the excerpt via your shortcode.
+`[list-pages excerpt="1"]`
+
+== Changelog ==
+
+= 1.7.7=
+
+* Fix sort order validation to allow for multiple sort columns.
+
+= 1.7.6 =
+
+* Security Update: Validate all shortcode attributes and escape output.
+
+= 1.7.5 =
+
+* Security Update: Sanitize HTML attributes before outputting.
+
+= 1.7.4 =
+
+* Fix fatal error: validate_list_type() needs to be public!
+
+= 1.7.3 =
+
+* Use PHP7 constructors.
+* Validate list type and convert `<li>` tags if not `<ul>` list type.
+* Checked WordPress 4.4.2 compatibility.
+
+= 1.7.2 =
+
+* Add short code arguments to the shortcode_list_pages_before/after actions.
+* Checked WordPress 4.2 compatibility.
+
+= 1.7.1 =
+
+* When no list type specified don't wrap in list tags.
+* Update List_Pages_Shortcode_Walker_Page class with changes made to the WordPress Walker_Page class.
+* Checked WordPress 3.9 compatibility.
+
+= 1.7 =
+
+* Add 'list-pages-shortcode' class to all lists.
+
+= 1.6 =
+
+* Add default arg values to start_el() Walker method. Props eceleste.
+* Added `shortcode_list_pages_before` action.
+* Added `shortcode_list_pages_after` action.
+* Added `list_pages_shortcode_item` filter.
+* Allow specifying of `post_type`.
+
+= 1.5 =
+
+* Added support for showing excerpt `[list-pages excerpt="1"]`.
+* Allow filtering of excerpt output using 'list_pages_shortcode_excerpt' filter.
+* Added support for outputting as ordered list `[list-pages list_type="ol"]`.
+* Allow HTML in 'title_li' attribute.
+
+= 1.4 =
+
+* Added support for 'post_status'.
+
+= 1.3 =
+
+* Added 'shortcode_list_pages_attributes' filter. Useful if you need to tweak any attributes based on context or current post type.
+* Allow 'child_of' to be overridden by shortcode parameter.
+
+= 1.2 =
+
+* Added shortcode support for 'exclude_current_page' parameter.
+* Added support for extra wp_list_pages() parameters: include, sort_order, meta_key, meta_value and offset.
+
+= 1.1 =
+
+* Added 'shortcode_list_pages' filter.
+* Added [sibling-pages] shortcode.
+
+= 1.0 =
+
+* First release.
+
+== Upgrade Notice ==
+
+
+= 1.7.7=
+
+Fix sort order validation to allow for multiple sort columns.
+
+= 1.7.6 =
+
+Security Update: Validate all shortcode attributes and escape output.
+
+= 1.7.5 =
+
+Security Update: Sanitize HTML attributes before outputting.
+
+= 1.7.2 =
+Add short code arguments to the shortcode_list_pages_before/after actions.
+
+= 1.7.1 =
+Update List_Pages_Shortcode_Walker_Page class with changes made to the WordPress Walker_Page class.
+
+= 1.7 =
+Add 'list-pages-shortcode' class to all lists.
+
+= 1.6 =
+Added `shortcode_list_pages_before` and `shortcode_list_pages_after` actions and `list_pages_shortcode_item` filter.
+
+= 1.5 =
+Added support for showing excerpt and filtering of excerpt output using 'list_pages_shortcode_excerpt' filter. Added support for outputting as ordered list.
+
+= 1.4 =
+Added support for 'post_status'.
+
+= 1.3 =
+Added 'shortcode_list_pages_attributes' filter and allow 'child_of' to be overridden by shortcode parameter.
+
+= 1.2 =
+Added support for extra wp_list_pages() parameters: include, sort_order, meta_key, meta_value and offset.
+
+= 1.1 =
+Added 'shortcode_list_pages' filter and [sibling-pages] shortcode.
