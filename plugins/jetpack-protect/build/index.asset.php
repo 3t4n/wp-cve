88b6044739ebe007dd9ab2f0b730b6d1cd4d979d
root <root@vultr>
2024-03-15 00:17:40 +0000

    Added plugin jetpack-protect

---
+++ plugins/jetpack-protect/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '69b5f82ff2ab6a921833');
